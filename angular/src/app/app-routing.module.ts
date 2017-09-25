050c94b4887089b82271f93c0a84cb0b6e97da90
Angela Xu <angela.xu@softwire.com>
2017-09-25 16:49:06 +0100

    Experimenting with adding another angular component

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageComponent } from './page/page.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 const routes: Routes = [
+    {
+        path: 'calculator',
+        component: CalculatorComponent
+    },
     {
         path: ':slug',
         component: PageComponent
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageComponent
+    PageComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
