84d8683d2d4b6465c6c7fff2dd930fbc4a333e80
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-12 14:07:30 +0100

    Small refactor

--- angular/src/app/questionnaire/questionnaire.component.ts
+++ angular/src/app/questionnaire/questionnaire.component.ts
@@ -1,5 +1,5 @@
-import { Component, AfterViewInit, ComponentFactoryResolver, ViewChild, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
-import { DUMMY_QUESTION_1, DUMMY_QUESTION_2, DUMMY_QUESTION_3, QuestionService } from "./questions/question.service";
+import { Component, AfterViewInit, ComponentFactoryResolver, ViewChild, ViewEncapsulation, ChangeDetectorRef, Input } from '@angular/core';
+import { QuestionService } from "./questions/question.service";
 import {Question} from "./questions/question";
 import {QuestionDirective} from "./questions/question.directive";
 import {oppositeDirection, QuestionBaseComponent, SlideInFrom} from "./questions/question.component";
@@ -12,7 +12,6 @@ import {oppositeDirection, QuestionBaseComponent, SlideInFrom} from "./questions
 })
 export class QuestionnaireComponent implements AfterViewInit {
 
-    private questionOrder: {key: string, heading: string}[];
     private currentQuestionIndex: number;
     private questionComponent: QuestionBaseComponent<any>;
     @ViewChild(QuestionDirective) questionHost: QuestionDirective;
@@ -20,23 +19,13 @@ export class QuestionnaireComponent implements AfterViewInit {
     constructor(private questionService: QuestionService,
                 private componentFactoryResolver: ComponentFactoryResolver,
                 private changeDetectorRef: ChangeDetectorRef) {
-        this.questionOrder = [
-            {key: DUMMY_QUESTION_1, heading: "Dummy question 1"},
-            {key: DUMMY_QUESTION_2, heading: "Dummy question 2"},
-            {key: DUMMY_QUESTION_3, heading: "Dummy question 3"}
-        ];
         this.currentQuestionIndex = 0;
     }
 
     ngAfterViewInit() {
         // Since we are inside a change detection hook, we can't just call this directly! We must
         // schedule it to be called after the change detection cycle has completed.
-        setTimeout(() =>  this.jumpToQuestion(this.findFirstUnansweredQuestionIndex()), 0);
-    }
-
-    private findFirstUnansweredQuestionIndex(): number {
-        const index = this.questionOrder.findIndex(q => this.questionService.getQuestion(q.key).response === undefined);
-        return index === -1 ? this.questionOrder.length - 1 : index;
+        setTimeout(() =>  this.jumpToQuestion(this.questionService.findFirstUnansweredQuestionIndex()), 0);
     }
 
     private jumpToQuestion(index) {
@@ -49,7 +38,7 @@ export class QuestionnaireComponent implements AfterViewInit {
     }
 
     private canGoForwards() {
-        return this.currentQuestionHasBeenAnswered() && this.currentQuestionIndex !== this.questionOrder.length - 1;
+        return this.questionService.questionHasBeenAnswered(this.currentQuestionIndex) && !this.questionService.isFinalQuestion(this.currentQuestionIndex);
     }
 
     private goBackOneQuestion() {
@@ -66,16 +55,8 @@ export class QuestionnaireComponent implements AfterViewInit {
         }
     }
 
-    private getCurrentQuestion(): Question<any, QuestionBaseComponent<any>> {
-        return this.questionService.getQuestion(this.questionOrder[this.currentQuestionIndex].key);
-    }
-
-    private currentQuestionHasBeenAnswered(): boolean {
-        return this.getCurrentQuestion().response !== undefined;
-    }
-
     private renderQuestion(slideInFrom: SlideInFrom) {
-        const question = this.getCurrentQuestion();
+        const question = this.questionService.getQuestionByIndex(this.currentQuestionIndex);
         const componentFactory = this.componentFactoryResolver.resolveComponentFactory(question.questionComponent);
 
         // Set the current question to slide out in the opposite direction. Manual change
@@ -93,7 +74,7 @@ export class QuestionnaireComponent implements AfterViewInit {
         this.questionComponent.slideInOut = slideInFrom;
         this.questionComponent.response = question.response;
         this.questionComponent.onResponse = response => {
-            this.getCurrentQuestion().response = this.questionComponent.response = response;
+            question.response = this.questionComponent.response = response;
             // Allow a change-detection cycle to run, and the user to see their updated answer,
             // before moving on to the next question.
             setTimeout(() => this.goForwardsOneQuestion(), 50);
--- angular/src/app/questionnaire/questions/question.service.ts
+++ angular/src/app/questionnaire/questions/question.service.ts
@@ -4,23 +4,50 @@ import {QuestionBaseComponent} from "./question.component";
 import {DummyQuestion} from "./dummy-question/dummy-question";
 import {DummyQuestionComponent} from "./dummy-question/dummy-question.component";
 
-export const DUMMY_QUESTION_1 = "dummy1";
-export const DUMMY_QUESTION_2 = "dummy2";
-export const DUMMY_QUESTION_3 = "dummy3";
+const DUMMY_QUESTION_1 = "dummy1";
+const DUMMY_QUESTION_2 = "dummy2";
+const DUMMY_QUESTION_3 = "dummy3";
 
 @Injectable()
 export class QuestionService {
 
-    public readonly questions: { [s: string]: Question<any, QuestionBaseComponent<any>>; };
+    private readonly questions: { [s: string]: Question<any, QuestionBaseComponent<any>>; };
+    private readonly questionOrder: {key: string, heading: string}[];
 
     constructor() {
         this.questions = {};
         this.questions[DUMMY_QUESTION_1] = new DummyQuestion(DummyQuestionComponent);
         this.questions[DUMMY_QUESTION_2] = new DummyQuestion(DummyQuestionComponent);
         this.questions[DUMMY_QUESTION_3] = new DummyQuestion(DummyQuestionComponent);
+        this.questionOrder = [
+            {key: DUMMY_QUESTION_1, heading: 'Dummy question 1'},
+            {key: DUMMY_QUESTION_2, heading: 'Dummy question 2'},
+            {key: DUMMY_QUESTION_3, heading: 'Dummy question 3'}
+        ];
     }
 
-    public getQuestion(name): Question<any, QuestionBaseComponent<any>> {
-        return this.questions[name];
+    private getQuestionByKey(key: string): Question<any, QuestionBaseComponent<any>> {
+        return this.questions[key];
+    }
+
+    public getQuestionByIndex(index: number): Question<any, QuestionBaseComponent<any>> {
+        return this.questions[this.questionOrder[index].key];
+    }
+
+    public getHeading(index: number): string {
+        return this.questionOrder[index].heading;
+    }
+
+    public isFinalQuestion(index: number): boolean {
+        return index === this.questionOrder.length - 1;
+    }
+
+    public questionHasBeenAnswered(index: number): boolean {
+        return this.getQuestionByIndex(index).response !== undefined;
+    }
+
+    public findFirstUnansweredQuestionIndex(): number {
+        const index = this.questionOrder.findIndex(q => this.getQuestionByKey(q.key).response === undefined);
+        return index === -1 ? this.questionOrder.length - 1 : index;
     }
 }
