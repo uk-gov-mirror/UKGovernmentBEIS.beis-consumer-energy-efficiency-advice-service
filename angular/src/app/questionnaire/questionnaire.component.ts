9d322909894f10f6c1c8574668e3fa77ab58c403
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-12 12:41:38 +0100

    Make slide animations possible from either side

--- angular/src/app/questionnaire/questionnaire.component.ts
+++ angular/src/app/questionnaire/questionnaire.component.ts
@@ -1,8 +1,8 @@
-import { Component, AfterViewInit, ComponentFactoryResolver, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, AfterViewInit, ComponentFactoryResolver, ViewChild, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
 import { DUMMY_QUESTION_1, DUMMY_QUESTION_2, DUMMY_QUESTION_3, QuestionService } from "./questions/question.service";
 import {Question} from "./questions/question";
 import {QuestionDirective} from "./questions/question.directive";
-import {QuestionBaseComponent} from "./questions/question.component";
+import {oppositeDirection, QuestionBaseComponent, SlideInFrom} from "./questions/question.component";
 
 @Component({
     selector: 'app-questionnaire',
@@ -14,9 +14,12 @@ export class QuestionnaireComponent implements AfterViewInit {
 
     private questionOrder: {key: string, heading: string}[];
     private currentQuestionIndex: number;
+    private questionComponent: QuestionBaseComponent<any>;
     @ViewChild(QuestionDirective) questionHost: QuestionDirective;
 
-    constructor(private questionService: QuestionService, private componentFactoryResolver: ComponentFactoryResolver) {
+    constructor(private questionService: QuestionService,
+                private componentFactoryResolver: ComponentFactoryResolver,
+                private changeDetectorRef: ChangeDetectorRef) {
         this.questionOrder = [
             {key: DUMMY_QUESTION_1, heading: "Dummy question 1"},
             {key: DUMMY_QUESTION_2, heading: "Dummy question 2"},
@@ -26,8 +29,9 @@ export class QuestionnaireComponent implements AfterViewInit {
     }
 
     ngAfterViewInit() {
-        // Since we are inside a change detection hook, we can't just call this directly!
-        setTimeout(() =>  this.goToQuestion(this.findFirstUnansweredQuestionIndex()), 0);
+        // Since we are inside a change detection hook, we can't just call this directly! We must
+        // schedule it to be called after the change detection cycle has completed.
+        setTimeout(() =>  this.jumpToQuestion(this.findFirstUnansweredQuestionIndex()), 0);
     }
 
     private findFirstUnansweredQuestionIndex(): number {
@@ -35,9 +39,9 @@ export class QuestionnaireComponent implements AfterViewInit {
         return index === -1 ? this.questionOrder.length - 1 : index;
     }
 
-    private goToQuestion(index) {
+    private jumpToQuestion(index) {
         this.currentQuestionIndex = index;
-        this.renderQuestion();
+        this.renderQuestion('none');
     }
 
     private canGoBack() {
@@ -50,13 +54,15 @@ export class QuestionnaireComponent implements AfterViewInit {
 
     private goBackOneQuestion() {
         if (this.canGoBack()) {
-            this.goToQuestion(this.currentQuestionIndex - 1);
+            this.currentQuestionIndex -= 1;
+            this.renderQuestion('left');
         }
     }
 
     private goForwardsOneQuestion() {
         if (this.canGoForwards()) {
-            this.goToQuestion(this.currentQuestionIndex + 1);
+            this.currentQuestionIndex += 1;
+            this.renderQuestion('right');
         }
     }
 
@@ -68,22 +74,29 @@ export class QuestionnaireComponent implements AfterViewInit {
         return this.getCurrentQuestion().response !== undefined;
     }
 
-    private renderQuestion() {
+    private renderQuestion(slideInFrom: SlideInFrom) {
         const question = this.getCurrentQuestion();
         const componentFactory = this.componentFactoryResolver.resolveComponentFactory(question.questionComponent);
 
-        const viewContainerRef = this.questionHost.viewContainerRef;
-        viewContainerRef.clear();
-
-        const componentRef = viewContainerRef.createComponent(componentFactory);
-        const componentInstance: QuestionBaseComponent<any> = componentRef.instance;
-
-        componentInstance.response = question.response;
-        componentInstance.onResponse = response => {
-            this.getCurrentQuestion().response = componentInstance.response = response;
-            // Allow a change-detection cycle to run, and the user to see their updated answer
-            // before moving on to the next question.
-            setTimeout(() => this.goForwardsOneQuestion(), 100);
+        // Set the current question to slide out in the opposite direction. Manual change
+        // detection must be triggered here or the change will not apply.
+        if (this.questionComponent) {
+            this.questionComponent.slideInOut = oppositeDirection(slideInFrom);
         }
+        this.changeDetectorRef.detectChanges();
+
+        // Instantiate the new question and slide in in from the given direction.
+        this.questionHost.viewContainerRef.clear();
+        const componentRef = this.questionHost.viewContainerRef.createComponent(componentFactory);
+        this.questionComponent = componentRef.instance;
+
+        this.questionComponent.slideInOut = slideInFrom;
+        this.questionComponent.response = question.response;
+        this.questionComponent.onResponse = response => {
+            this.getCurrentQuestion().response = this.questionComponent.response = response;
+            // Allow a change-detection cycle to run, and the user to see their updated answer,
+            // before moving on to the next question.
+            setTimeout(() => this.goForwardsOneQuestion(), 50);
+        };
     }
 }
--- angular/src/app/questionnaire/questions/question.component.ts
+++ angular/src/app/questionnaire/questions/question.component.ts
@@ -1,29 +1,51 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input, HostBinding, ViewEncapsulation } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
 declare const Reflect: any;
 
+export type SlideInFrom = 'none' | 'left' | 'right';
+
+export function oppositeDirection(slideInFrom: SlideInFrom): SlideInFrom {
+    if (slideInFrom === 'none') {
+        return 'none';
+    } else if (slideInFrom === 'left') {
+        return 'right';
+    } else if (slideInFrom === 'right') {
+        return 'left';
+    }
+}
+
 const slideInOutAnimation = trigger('slideInOut', [
-    state('default', style({transform: 'translateX(0)'})),
-    transition(':enter', [
+    state('none', style({transform: 'translateX(0)'})),
+    state('left', style({transform: 'translateX(0)'})),
+    state('right', style({transform: 'translateX(0)'})),
+    transition('void => left', [
+        style({transform: 'translateX(-100vw)'}),
+        animate(500)
+    ]),
+    transition('void => right', [
         style({transform: 'translateX(100vw)'}),
         animate(500)
     ]),
-    transition(':leave', [
+    transition('left => void', [
         animate(500, style({transform: 'translateX(-100vw)'}))
+    ]),
+    transition('right => void', [
+        animate(500, style({transform: 'translateX(100vw)'}))
     ])
 ]);
 
 export function QuestionComponent(metadata: any = {}) {
     return function(cls: any) {
         const annotations = Reflect.getMetadata('annotations', cls) || [];
-        annotations.push(new Component({...metadata, encapsulation: ViewEncapsulation.None, animations: [slideInOutAnimation], host: {'[@slideInOut]': 'default'}}));
+        annotations.push(new Component({...metadata, encapsulation: ViewEncapsulation.None, animations: [slideInOutAnimation]}));
         Reflect.defineMetadata('annotations', annotations, cls);
         return cls;
     };
 }
 
-export class QuestionBaseComponent<S> {
+export abstract class QuestionBaseComponent<S> {
+    @HostBinding('@slideInOut')@Input() slideInOut: string = 'none';
     @Input() response: S;
     @Input() onResponse: (response: S) => void;
 }
