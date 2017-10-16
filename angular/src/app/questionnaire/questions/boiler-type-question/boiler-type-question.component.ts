e8d30d73e7ae69cc3aeb04b166d9962d13b6e723
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-16 16:39:46 +0100

    Sort out animation styling issue

--- angular/src/app/questionnaire/questions/boiler-type-question/boiler-type-question.component.ts
+++ angular/src/app/questionnaire/questions/boiler-type-question/boiler-type-question.component.ts
@@ -4,7 +4,8 @@ import {QuestionBaseComponent, slideInOutAnimation} from "../question.component"
 @Component({
     selector: 'app-boiler-type-question',
     templateUrl: './boiler-type-question.component.html',
-    styleUrls: ['./boiler-type-question.component.scss']
+    styleUrls: ['./boiler-type-question.component.scss'],
+    animations: [slideInOutAnimation],
 })
 export class BoilerTypeQuestionComponent extends QuestionBaseComponent<boolean> {
 }
