716a6556a2db8874096a46ccce23d5d92453f98c
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-16 17:28:28 +0100

    Add some simple tests for boiler type question

--- angular/src/app/questionnaire/questions/boiler-type-question/boiler-type-question.component.spec.ts
+++ angular/src/app/questionnaire/questions/boiler-type-question/boiler-type-question.component.spec.ts
@@ -1,25 +1,54 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 
-import { BoilerTypeQuestionComponent } from './boiler-type-question.component';
+import {BoilerTypeQuestionComponent} from './boiler-type-question.component';
+import {ResponseData} from "../response-data";
+import {BoilerTypeQuestion} from "./boiler-type-question";
 
 describe('BoilerTypeQuestionComponent', () => {
-  let component: BoilerTypeQuestionComponent;
-  let fixture: ComponentFixture<BoilerTypeQuestionComponent>;
+    let component: BoilerTypeQuestionComponent;
+    let fixture: ComponentFixture<BoilerTypeQuestionComponent>;
+    let responseData: ResponseData;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BoilerTypeQuestionComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ BoilerTypeQuestionComponent ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BoilerTypeQuestionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BoilerTypeQuestionComponent);
+        component = fixture.componentInstance;
+        responseData = new ResponseData();
+        component.question = new BoilerTypeQuestion(responseData);
+        component.notifyOfCompletion = jasmine.createSpy('notifyOfCompletion');
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should set the response when clicking on one of the buttons', () => {
+        // given
+
+        // when
+        let yes = fixture.debugElement.query(By.css('#yesButton'));
+        yes.nativeElement.click();
+
+        // then
+        expect(responseData.condensingBoiler).toBe(true);
+    });
+
+    it('should notify of completion when clicking on one of the buttons', () => {
+        // given
+
+        // when
+        let no = fixture.debugElement.query(By.css('#noButton'));
+        no.nativeElement.click();
+
+        // then
+        expect(component.notifyOfCompletion).toHaveBeenCalled();
+    });
 });
