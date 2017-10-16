8c11da25f08b64b37a91dbf53dd73d9c71fe309d
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-16 10:53:34 +0100

    Add simple tests for flat type question

--- angular/src/app/questionnaire/questions/flat-position-question/flat-position-question.component.spec.ts
+++ angular/src/app/questionnaire/questions/flat-position-question/flat-position-question.component.spec.ts
@@ -1,25 +1,55 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { FlatPositionQuestionComponent } from './flat-position-question.component';
+import {ResponseData} from "../response-data";
+import {FlatPositionQuestion} from "./flat-position-question";
+import {FlatPosition} from "./flat-position";
 
 describe('FlatPositionQuestionComponent', () => {
-  let component: FlatPositionQuestionComponent;
-  let fixture: ComponentFixture<FlatPositionQuestionComponent>;
+    let fixture: ComponentFixture<FlatPositionQuestionComponent>;
+    let component: FlatPositionQuestionComponent;
+    let responseData: ResponseData;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FlatPositionQuestionComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ FlatPositionQuestionComponent ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FlatPositionQuestionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FlatPositionQuestionComponent);
+        component = fixture.componentInstance;
+        responseData = new ResponseData();
+        component.question = new FlatPositionQuestion(responseData);
+        component.notifyOfCompletion = jasmine.createSpy('notifyOfCompletion');
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should set the response when clicking on a flat position', () => {
+        // given
+
+        // when
+        let threeSidesExposed = fixture.debugElement.query(By.css('.three-sides-exposed'));
+        threeSidesExposed.nativeElement.click();
+
+        // then
+        expect(responseData.flatPosition).toBe(FlatPosition.ThreeSidesExposed);
+    });
+
+    it('should notify of completion when clicking on a flat position', () => {
+        // given
+
+        // when
+        let threeSidesExposed = fixture.debugElement.query(By.css('.three-sides-exposed'));
+        threeSidesExposed.nativeElement.click();
+
+        // then
+        expect(component.notifyOfCompletion).toHaveBeenCalled();
+    });
 });
