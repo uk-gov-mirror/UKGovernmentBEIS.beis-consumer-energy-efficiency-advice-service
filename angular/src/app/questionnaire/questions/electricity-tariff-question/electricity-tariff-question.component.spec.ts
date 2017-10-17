d743387f073f402014a2047475a8a1a3f8c25fa1
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-17 10:19:39 +0100

    Add a test for electricity tariff question

--- angular/src/app/questionnaire/questions/electricity-tariff-question/electricity-tariff-question.component.spec.ts
+++ angular/src/app/questionnaire/questions/electricity-tariff-question/electricity-tariff-question.component.spec.ts
@@ -1,25 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 
-import { ElectricityTariffQuestionComponent } from './electricity-tariff-question.component';
+import {ElectricityTariffQuestionComponent} from './electricity-tariff-question.component';
+import {ResponseData} from "../response-data";
+import {ElectricityTariffQuestion} from "./electricity-tariff-question";
+import {ElectricityTariff} from "./electricity-tariff";
 
 describe('ElectricityTariffQuestionComponent', () => {
-  let component: ElectricityTariffQuestionComponent;
-  let fixture: ComponentFixture<ElectricityTariffQuestionComponent>;
+    let fixture: ComponentFixture<ElectricityTariffQuestionComponent>;
+    let component: ElectricityTariffQuestionComponent;
+    let responseData: ResponseData;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ElectricityTariffQuestionComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ElectricityTariffQuestionComponent ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ElectricityTariffQuestionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ElectricityTariffQuestionComponent);
+        component = fixture.componentInstance;
+        responseData = new ResponseData();
+        component.question = new ElectricityTariffQuestion(responseData);
+        component.notifyOfCompletion = jasmine.createSpy('notifyOfCompletion');
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should set the response when clicking on a tariff type', () => {
+        // given
+
+        // when
+        let standard = fixture.debugElement.query(By.css('#standardButton'));
+        standard.nativeElement.click();
+
+        // then
+        expect(responseData.electricityTariff).toBe(ElectricityTariff.Standard);
+    });
+
+    it('should notify of completion when clicking on a tariff type', () => {
+        // given
+
+        // when
+        let offPeak = fixture.debugElement.query(By.css('#offPeakButton'));
+        offPeak.nativeElement.click();
+
+        // then
+        expect(component.notifyOfCompletion).toHaveBeenCalled();
+    });
 });
