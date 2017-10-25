5c6262df15c184ae8dc1e735b33becc2c3a353cf
Angela Xu <angela.xu@softwire.com>
2017-10-25 15:46:09 +0100

    BEISDEAS-32: Clear cache on saving data

--- wordpress/wp-content/themes/angular-theme/functions/configure_api_caching.php
+++ wordpress/wp-content/themes/angular-theme/functions/configure_api_caching.php
@@ -4,3 +4,9 @@ add_filter('rest_cache_headers', function ($headers) {
     $headers['Cache-Control'] = 'public, max-age=3600';
     return $headers;
 });
+
+add_action('save_post', function () {
+    if (class_exists('WP_REST_Cache')) {
+        WP_REST_Cache::empty_cache();
+    }
+});
