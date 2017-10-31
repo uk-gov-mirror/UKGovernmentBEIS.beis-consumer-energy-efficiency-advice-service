55f47cb7cb8bd13fbb0bb0e64ad2848334fd8997
Adam Smith <adam.smith@softwire.com>
2017-10-31 16:06:41 +0000

    BEISDEAS-35 Review markups: use innerHTML binding instead of unescaping HTML chars

--- angular/src/app/common/header/wordpress-pages-service/wordpress-page.ts
+++ angular/src/app/common/header/wordpress-pages-service/wordpress-page.ts
@@ -1,17 +1,11 @@
 import {WordpressPageResponse} from './wordpress-page-response';
-import {unescape} from 'lodash-es';
 
 export class WordpressPage {
     public title: string;
     public slug: string;
 
     constructor(wordpressPageResponse: WordpressPageResponse) {
-        this.title = WordpressPage.decodeEscapedAsciiChars(wordpressPageResponse.title.rendered);
+        this.title = wordpressPageResponse.title.rendered;
         this.slug = wordpressPageResponse.slug;
-    }
-
-    private static decodeEscapedAsciiChars(input: string) {
-        // return input.replace(/&#(\d+);/g, (m, n) => String.fromCharCode(n));
-        return unescape(input);
     }
 }
