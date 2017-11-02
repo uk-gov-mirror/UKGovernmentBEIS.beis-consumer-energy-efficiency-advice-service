34cf8ccb651f36131f87f8b031f693a8f0944713
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-11-02 16:16:37 +0000

    Remove demo response summary component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -10,7 +10,6 @@ import {AppComponent} from "./app.component";
 import {QuestionnaireModule} from "./questionnaire/questionnaire.module";
 import {SharedModule} from "./shared/shared.module";
 import {ResultsPageModule} from "./results-page/results-page.module";
-import {ResponseSummaryComponent} from "./response-summary/response-summary.component";
 import {HomePageModule} from "./home-page/home-page.module";
 import {LandingPageModule} from "./landing-page/landing-page.module";
 import {LayoutComponentsModule} from "./layout-components/layout-components.module";
@@ -18,7 +17,6 @@ import {LayoutComponentsModule} from "./layout-components/layout-components.modu
 @NgModule({
     declarations: [
         AppComponent,
-        ResponseSummaryComponent,
     ],
     imports: [
         BrowserModule,
--- angular/src/app/shared/response-data/response-data.ts
+++ angular/src/app/shared/response-data/response-data.ts
@@ -7,27 +7,8 @@ import {HomeAge} from "../../questionnaire/questionnaires/home-basics/questions/
 import {HomeType} from "../../questionnaire/questionnaires/home-basics/questions/home-type-question/home-type";
 import {RdSapInput} from "../energy-calculation-api-service/request/rdsap-input";
 
-interface ResponseDataShape {
-    postcode: any;
-    epc: any;
-    localAuthorityCode: any;
-    confirmEpc: boolean;
-    homeType: any;
-    homeAge: any;
-    flatPosition: any;
-    numberOfStoreys: any;
-    numberOfBedrooms: any;
-    fuelType: any;
-    condensingBoiler: any;
-    electricityTariff: any;
-    numberOfOccupants: any;
-    numberOfFridgeFreezers: any;
-    numberOfFridges: any;
-    numberOfFreezers: any;
-}
-
 @Injectable()
-export class ResponseData implements ResponseDataShape {
+export class ResponseData {
     public postcode: string;
     public epc: Epc;
     public localAuthorityCode: string;
@@ -44,54 +25,6 @@ export class ResponseData implements ResponseDataShape {
     public numberOfFridgeFreezers: number;
     public numberOfFridges: number;
     public numberOfFreezers: number;
-}
-
-// This class is *ONLY* used for displaying the summary page of the user's responses.
-// It is necessary because at runtime, some values (such as enum values, which are
-// just numbers at runtime) display in a way which is meaningless.
-//
-// This class therefore is of the same shape as ResponseData, but transforms certain
-// values into forms which will display correctly.
-export class DisplayableResponseData implements ResponseDataShape {
-    //noinspection JSUnusedGlobalSymbols
-    constructor(public postcode: string,
-                public epc: Epc,
-                public localAuthorityCode: string,
-                public confirmEpc: boolean,
-                public homeType: string,
-                public homeAge: string,
-                public flatPosition: string,
-                public numberOfStoreys: number,
-                public numberOfBedrooms: number,
-                public fuelType: string,
-                public condensingBoiler: boolean,
-                public electricityTariff: string,
-                public numberOfOccupants: number,
-                public numberOfFridgeFreezers: number,
-                public numberOfFridges: number,
-                public numberOfFreezers: number) {
-    }
-
-    static fromResponseData(responseData: ResponseData): DisplayableResponseData {
-        return new DisplayableResponseData(
-            responseData.postcode,
-            responseData.epc,
-            responseData.localAuthorityCode,
-            responseData.confirmEpc,
-            HomeType[responseData.homeType],
-            HomeAge[responseData.homeAge],
-            FlatPosition[responseData.flatPosition],
-            responseData.numberOfStoreys,
-            responseData.numberOfBedrooms,
-            FuelType[responseData.fuelType],
-            responseData.condensingBoiler,
-            ElectricityTariff[responseData.electricityTariff],
-            responseData.numberOfOccupants,
-            responseData.numberOfFridgeFreezers,
-            responseData.numberOfFridges,
-            responseData.numberOfFreezers,
-        );
-    }
 }
 
 export function isComplete(responseData: ResponseData) {
