9267f35a00d081d6d88ce74ee61ae32bd9128628
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-11-06 16:43:56 +0000

    Some merge conflict fixes

--- angular/src/app/results-page/results-page.module.ts
+++ angular/src/app/results-page/results-page.module.ts
@@ -10,6 +10,7 @@ import {EnergyCalculationApiService} from "../shared/energy-calculation-api-serv
 import {LocalAuthorityService} from "./local-authority-service/local-authority.service";
 import {RoutingModule} from "../app-routing.module";
 import {SharedModule} from "../shared/shared.module";
+import {RecommendationService} from "./recommendation-service/recommendation.service";
 
 @NgModule({
     declarations: [
@@ -29,7 +30,8 @@ import {SharedModule} from "../shared/shared.module";
     ],
     providers: [
         EnergyCalculationApiService,
-        LocalAuthorityService
+        LocalAuthorityService,
+        RecommendationService,
     ]
 })
 export class ResultsPageModule {
