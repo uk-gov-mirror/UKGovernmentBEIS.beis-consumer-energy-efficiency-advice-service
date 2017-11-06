ceca14fba0ce27a1ab28950e8d0717e1b1e8471f
Angela Xu <angela.xu@softwire.com>
2017-11-06 17:02:49 +0000

    BEISDEAS-53: Do not go into start on questionnaire if complete

--- angular/src/app/home-page/home-page.component.spec.ts
+++ angular/src/app/home-page/home-page.component.spec.ts
@@ -7,12 +7,14 @@ import {By} from "@angular/platform-browser";
 import {UserJourneyType} from "../shared/response-data/user-journey-type";
 import {Location} from "@angular/common";
 import {NavigationBarComponent} from "../layout-components/navigation-bar/navigation-bar.component";
+import {QuestionnaireService} from "../questionnaire/questionnaire.service";
 
 describe('HomePageComponent', () => {
     let component: HomePageComponent;
     let fixture: ComponentFixture<HomePageComponent>;
 
     let responseDataStub: ResponseData;
+    let questionnaireComplete: boolean;
 
     class DummyComponent {
     }
@@ -21,8 +23,16 @@ describe('HomePageComponent', () => {
         responseDataStub = new ResponseData();
         TestBed.configureTestingModule({
             declarations: [HomePageComponent, LatestNewsCardComponent, NavigationBarComponent],
-            imports: [RouterTestingModule.withRoutes([{path: 'questionnaire/home-basics', component: DummyComponent}])],
-            providers: [{provide: ResponseData, useValue: responseDataStub}]
+            imports: [RouterTestingModule.withRoutes([
+                {path: 'questionnaire/home-basics', component: DummyComponent},
+                {path: 'results', component: DummyComponent}
+            ])],
+            providers: [
+                {provide: ResponseData, useValue: responseDataStub},
+                {
+                    provide: QuestionnaireService,
+                    useValue: {isComplete: () => questionnaireComplete}
+                }]
         })
             .compileComponents();
     }));
@@ -37,7 +47,10 @@ describe('HomePageComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should set the user journey type and move to the questionnaire when the appropriate button is clicked', async () => {
+    it('should set the user journey type and move to the questionnaire when the appropriate button is clicked, if questionnaire incomplete', async () => {
+        // given
+        questionnaireComplete = false;
+
         // when
         const button = fixture.debugElement.query(By.css('.calculator-button'));
         button.nativeElement.click();
@@ -48,5 +61,21 @@ describe('HomePageComponent', () => {
             const location = fixture.debugElement.injector.get(Location);
             expect(location.path()).toBe('/questionnaire/home-basics');
         });
-    })
+    });
+
+    it('should set the user journey type and move to the results page when the appropriate button is clicked, if questionnaire complete', async () => {
+        // given
+        questionnaireComplete = true;
+
+        // when
+        const button = fixture.debugElement.query(By.css('.calculator-button'));
+        button.nativeElement.click();
+
+        // then
+        expect(responseDataStub.userJourneyType).toBe(UserJourneyType.HomepageLink);
+        fixture.whenStable().then(() => {
+            const location = fixture.debugElement.injector.get(Location);
+            expect(location.path()).toBe('/results');
+        });
+    });
 });
--- angular/src/app/home-page/home-page.component.ts
+++ angular/src/app/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {UserJourneyType} from "../shared/response-data/user-journey-type";
 import {ResponseData} from "../shared/response-data/response-data";
 import {Router} from "@angular/router";
+import {QuestionnaireService} from "../questionnaire/questionnaire.service";
 
 @Component({
     selector: 'app-home-page',
@@ -10,11 +11,13 @@ import {Router} from "@angular/router";
 })
 export class HomePageComponent {
     constructor(private responseData: ResponseData,
+                private questionnaireService: QuestionnaireService,
                 private router: Router) {
     }
 
     onEnergyCalculatorButtonClick() {
         this.responseData.userJourneyType = UserJourneyType.HomepageLink;
-        this.router.navigate(['/questionnaire/home-basics']);
+        const route = this.questionnaireService.isComplete('home-basics') ? '/results' : '/questionnaire/home-basics';
+        this.router.navigate([route]);
     }
 }
