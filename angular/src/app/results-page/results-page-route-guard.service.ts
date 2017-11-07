ada033fd9782bdb14aa23f3c52791ecc9cb7c725
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-11-07 00:54:28 +0000

    Undo changes to results guard from testing

--- angular/src/app/results-page/results-page-route-guard.service.ts
+++ angular/src/app/results-page/results-page-route-guard.service.ts
@@ -13,28 +13,6 @@ export class ResultsPageRouteGuard implements CanActivate {
     }
 
     canActivate() {
-        Object.assign(this.responseData, {
-            postcode: 'se20ul',
-            epc: null,
-            localAuthorityCode: 'E09000011',
-            confirmEpc: true,
-            homeowner: true,
-            homeType: HomeType.MidTerraceHouse,
-            homeAge: HomeAge.pre1900,
-            flatPosition: undefined,
-            numberOfStoreys: 1,
-            numberOfBedrooms: 1,
-            fuelType: FuelType.MainsGas,
-            condensingBoiler: true,
-            electricityTariff: undefined,
-            numberOfOccupants: 2,
-            numberOfShowersPerWeek: 2,
-            numberOfBathsPerWeek: 7,
-            numberOfFridgeFreezers: 1,
-            numberOfFridges: 0,
-            numberOfFreezers: 0,
-            livingRoomTemperature: 18
-        });
         if (isComplete(this.responseData)) {
             return true;
         }
