d0b2964050bf97a6bd7dd6aaf5485f4160b94712
Adam Smith <adam.smith@softwire.com>
2017-11-13 09:25:56 +0000

    BEISDEAS-57 Review markups: improve regexes

--- node-scripts/boiler-pcdf-parser/src/service/pcdf-parser.js
+++ node-scripts/boiler-pcdf-parser/src/service/pcdf-parser.js
@@ -4,9 +4,9 @@ var pcdfParser = {};
 
 pcdfParser.getPcdfWithIgnoredLinesStripped = function (pcdf) {
     // Match anything up to and including the first line which begins "$001"
-    var matchIgnoredLinesBeforeStartOfData = /^[\s\S]*\s+\$001.*\s+/;
+    var matchIgnoredLinesBeforeStartOfData = /^.*\$001\S*\s+/s;
     // Match anything from the first line which begins "$999" onwards
-    var matchIgnoredLinesAfterEndOfData = /\$999.*[\s\S]*$/;
+    var matchIgnoredLinesAfterEndOfData = /\$999.*$/s;
     // Match any line which begins with #
     var matchCommentLines = /\s*#(.*)/g;
     return pcdf
@@ -48,12 +48,12 @@ function getTableBodyCsvString(pcdf, tableNumber, expectedDataFormatNumber) {
 function getRegexForTableNumber(tableNumber) {
     // The start of table is indicated by a 'control line' which begins with $X,Y
     // where X is the table number and Y is the data format number
-    var regexSourceForTableStartLine = '\\\s*\\\$' + tableNumber + ',(\\\d+).*\\\s*';
+    var regexForTableStartLine = new RegExp('\\\s*\\\$' + tableNumber + ',(\\\d+).*\\\s*');
     // The succeeding lines (which do not begin with $) form the table body
-    var regexSourceForTableBodyLines = '((?:[^\\\$]\\\S*\\\s*)*)';
+    var regexForTableBodyLines = /((?:[^\$]\S*\s*)*)/;
     // The table is terminated by the next line which begins with $
-    var regexSourceForTableEndLine = '\\\s*\\\$';
-    return new RegExp(regexSourceForTableStartLine + regexSourceForTableBodyLines + regexSourceForTableEndLine, "g");
+    var regexForTableEndLine = /\s*\$/;
+    return new RegExp(regexForTableStartLine.source + regexForTableBodyLines.source + regexForTableEndLine.source, "g");
 }
 
 function verifyDataFormatNumber(expectedDataFormatNumber, actualDataFormatNumber, tableNumber) {
