e5a71ddf59cc191c1f1a6bc1a185eac5e8b14b59
Adam Smith <adam.smith@softwire.com>
2017-11-15 10:51:17 +0000

    BEISDEAS-86 Grants landing page layout and styles

--- angular/src/app/grants/grants.module.ts
+++ angular/src/app/grants/grants.module.ts
@@ -1,21 +1,26 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 import {SharedModule} from "../shared/shared.module";
 import {GrantsQuestionnaireComponent} from "./grants-questionnaire/grants-questionnaire.component";
 import {QuestionnaireModule} from "../questionnaire/questionnaire.module";
+import {GrantsLandingPageComponent} from "./landing-page/grants-landing-page.component";
 
 @NgModule({
     declarations: [
-        GrantsQuestionnaireComponent
+        GrantsQuestionnaireComponent,
+        GrantsLandingPageComponent
     ],
     exports: [
-        GrantsQuestionnaireComponent
+        GrantsQuestionnaireComponent,
+        GrantsLandingPageComponent
     ],
     imports: [
         SharedModule,
         CommonModule,
-        QuestionnaireModule
+        QuestionnaireModule,
+        FormsModule
     ]
 })
 export class GrantsModule {
