a99df87796395e3a62eef56793c7c818cd4a1a2e
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-11-15 22:34:57 +0000

    Commit forgotten WP files...

--- wordpress/wp-content/themes/angular-theme/functions/epc.php
+++ wordpress/wp-content/themes/angular-theme/functions/epc.php
@@ -14,12 +14,30 @@ function get_epc(WP_REST_Request $request)
         'address' => $request['address'],
         'size' => $request['size']
     ), 'https://epc.opendatacommunities.org/api/v1/domestic/search?');
-    return response_body_or_else_error(wp_remote_get($url, $args), 'EPC_API_ERROR');
+    return response_body_or_else_error(wp_remote_get($url, $args), 'EPC_SEARCH_API_ERROR');
+}
+
+// Add Wordpress API function for getting EPC recommendations
+function get_epc_recommendations(WP_REST_Request $request)
+{
+    $args = array(
+        'headers' => array(
+            'Authorization' => 'Basic ' . 'YW5nZWxhLnh1QHNvZnR3aXJlLmNvbTplNzMxNTI3YmRlMzc1NTc1OWFjNTlmMTBlZTRjZGRkZGJiYWNmMjU4',
+            'Accept' => 'application/json'
+        )
+    );
+    $lmkKey = $request['lmkKey'];
+    $url = "https://epc.opendatacommunities.org/api/v1/domestic/recommendations/$lmkKey";
+    return response_body_or_else_error(wp_remote_get($url, $args), 'EPC_RECOMMENDATIONS_API_ERROR');
 }
 
 add_action('rest_api_init', function () {
     register_rest_route('angular-theme/v1', '/epc', array(
         'methods' => 'GET',
         'callback' => 'get_epc',
+    ));
+    register_rest_route('angular-theme/v1', '/epc-recommendations', array(
+        'methods' => 'GET',
+        'callback' => 'get_epc_recommendations',
     ));
 });
