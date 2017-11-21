efc8c9cf707c4817f0037c28379b78686d435ff0
Bob Chen <Bob.Chen@Softwire.com>
2017-11-21 14:56:16 +0000

    BEISDEAS-99: REfactor questionHeadingProcessor

--- angular/src/app/questionnaire/questionHeadingProcessor.service.ts
+++ angular/src/app/questionnaire/questionHeadingProcessor.service.ts
@@ -3,19 +3,42 @@ import {ResponseData} from "../shared/response-data/response-data";
 import {getFuelTypeDescription} from "./questions/fuel-type-question/fuel-type";
 import {getHomeTypeDescription} from "./questions/home-type-question/home-type";
 
+export interface QuestionHeadingReplacement {
+    placeholder: RegExp,
+    replacementFinder: (ResponseData) => string
+}
+
 @Injectable()
 export class QuestionHeadingProcessor {
     private static readonly FUEL_TYPE_PLACEHOLDER = '{{fuel_type}}';
     private static readonly PROPERTY_PLACEHOLDER = '{{property}}';
 
+    private static readonly QUESTION_HEADING_REPLACEMENTS: QuestionHeadingReplacement[] =
+        [
+            {
+                placeholder: new RegExp(QuestionHeadingProcessor.FUEL_TYPE_PLACEHOLDER, 'g'),
+                replacementFinder: (responseData: ResponseData): string => {
+                    return getFuelTypeDescription(responseData.fuelType);
+                }
+            },
+            {
+                placeholder: new RegExp(QuestionHeadingProcessor.PROPERTY_PLACEHOLDER, 'g'),
+                replacementFinder: (responseData: ResponseData): string => {
+                    return getHomeTypeDescription(responseData.homeType);
+                }
+            }
+        ];
+
     constructor(private responseData: ResponseData) {
     }
 
     public replacePlaceholders(questionHeading) {
-        const fuelTypeRegex = new RegExp(QuestionHeadingProcessor.FUEL_TYPE_PLACEHOLDER, 'g');
-        const propertyRegex = new RegExp(QuestionHeadingProcessor.PROPERTY_PLACEHOLDER, 'g');
+        QuestionHeadingProcessor.QUESTION_HEADING_REPLACEMENTS.forEach(
+            replacement => {
+                questionHeading = questionHeading.replace(replacement.placeholder, replacement.replacementFinder(this.responseData))
+            }
+        );
 
-        return questionHeading.replace(fuelTypeRegex, getFuelTypeDescription(this.responseData.fuelType))
-            .replace(propertyRegex, getHomeTypeDescription(this.responseData.homeType));
+        return questionHeading;
     }
 }
