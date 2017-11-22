7bf39f2a9f323eccad1bdf9251215153e36b5d5b
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-11-22 11:58:59 +0000

    Add a page for boiler replacements

--- angular/src/app/boiler/replacement-page/boiler-replacement-page.component.ts
+++ angular/src/app/boiler/replacement-page/boiler-replacement-page.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from "@angular/core";
+import {BoilerTypesService} from "../boiler-types-service/boiler-types.service";
+import {BoilerType} from "../boiler-types-service/boiler-type";
 
 @Component({
     selector: 'app-boiler-replacement-page',
@@ -7,9 +9,24 @@ import {Component, OnInit} from "@angular/core";
 })
 export class BoilerReplacementPageComponent implements OnInit {
 
-    constructor() { }
+    loading: boolean = true;
+    error: boolean = false;
+    boilers: BoilerType[];
+
+    constructor(private boilerTypesService: BoilerTypesService) {
+    }
 
     ngOnInit() {
+        this.boilerTypesService.fetchBoilerTypes().subscribe(
+            boilerTypes => this.boilers = Object.values(boilerTypes),
+            () => this.handleError(),
+            () => this.loading = false,
+        );
+    }
+
+    private handleError() {
+        this.error = true;
+        this.loading = false;
     }
 
 }
