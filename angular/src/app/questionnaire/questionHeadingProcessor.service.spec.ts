5170866c82daf1a74dbce4826b6ffff2c2e27400
Bob Chen <Bob.Chen@Softwire.com>
2017-11-22 12:48:06 +0000

    BEISDEAS-99: Markups

--- angular/src/app/questionnaire/questionHeadingProcessor.service.spec.ts
+++ angular/src/app/questionnaire/questionHeadingProcessor.service.spec.ts
@@ -1,6 +1,5 @@
 import {TestBed, getTestBed} from "@angular/core/testing"
 
-import {HttpTestingController} from "@angular/common/http/testing";
 import {QuestionHeadingProcessor} from "./questionHeadingProcessor.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {ResponseData} from "../shared/response-data/response-data";
@@ -8,15 +7,13 @@ import {FuelType} from "./questions/fuel-type-question/fuel-type";
 import {HomeType} from "./questions/home-type-question/home-type";
 
 fdescribe('questionHeadingProcessor', () => {
-    let httpMock: HttpTestingController;
     let injector: TestBed;
     let service: QuestionHeadingProcessor;
     let responseData: ResponseData;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [QuestionHeadingProcessor, ResponseData],
-            imports: [HttpClientTestingModule]
+            providers: [QuestionHeadingProcessor, ResponseData]
         });
         injector = getTestBed();
         service = TestBed.get(QuestionHeadingProcessor);
@@ -30,7 +27,7 @@ fdescribe('questionHeadingProcessor', () => {
     });
 
     describe('#replacePlaceholders', () => {
-        it('replaces fuel type placeholder in question', () => {
+        it('replaces fuel type placeholder in aquestion', () => {
             // given
             const question = 'What do you pay a month for your {{fuel_type}}?';
             responseData.fuelType = FuelType.Electricity;
@@ -42,7 +39,7 @@ fdescribe('questionHeadingProcessor', () => {
             expect(processedQuestion).toBe('What do you pay a month for your electricity?');
         });
 
-        it('replaces multiple placeholders in question', () => {
+        it('replaces multiple placeholders in a question', () => {
             // given
             const question = 'Is your fuel type {{fuel_type}} and your property {{property}}?';
             responseData.fuelType = FuelType.Electricity;
@@ -55,7 +52,7 @@ fdescribe('questionHeadingProcessor', () => {
             expect(processedQuestion).toBe('Is your fuel type electricity and your property top floor flat?');
         });
 
-        it('replaces placeholder which appears multiple times in question', () => {
+        it('replaces placeholder that appears multiple times in a question', () => {
             // given
             const question = 'You have indicated that you use {{fuel_type}}. Is {{fuel_type}} what you actually want to use?';
             responseData.fuelType = FuelType.Electricity;
