880273b24df273601aa07deed675e180a6820e46
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-03-15 17:32:25 +0000

    BEISDEAS-191 Add reference generation, and return a 201 "Created" from a successful creation

--- user-site-server/src/main/java/uk/gov/beis/dceas/controller/UserStateController.java
+++ user-site-server/src/main/java/uk/gov/beis/dceas/controller/UserStateController.java
@@ -1,9 +1,17 @@
 package uk.gov.beis.dceas.controller;
 
 import org.jooq.DSLContext;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import uk.gov.beis.dceas.api.UserState;
-import uk.gov.beis.dceas.db.gen.tables.records.UserStateRecord;
 
 import java.sql.Timestamp;
 import java.time.Instant;
@@ -14,6 +22,9 @@ import static uk.gov.beis.dceas.spring.NotFoundException.notFoundIfNull;
 @RestController
 @RequestMapping("/api/userState")
 public class UserStateController {
+
+    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int REFERENCE_CHARACTER_LENGTH = 8;
 
     private final DSLContext dslContext;
 
@@ -32,10 +43,9 @@ public class UserStateController {
         );
     }
 
-    @PostMapping("/")
-    public void createUserState(@RequestBody String state) {
-        // TODO BEISDEAS-191 Generate reference
-        String reference = "something";
+    @PostMapping
+    public ResponseEntity<?> createUserState(UriComponentsBuilder builder, @RequestBody String state) {
+        String reference = generateReference();
         dslContext
             .insertInto(USER_STATE, USER_STATE.REFERENCE, USER_STATE.STATE, USER_STATE.UPDATED)
             .values(
@@ -43,6 +53,10 @@ public class UserStateController {
                 state,
                 Timestamp.from(Instant.now()))
             .execute();
+
+        // Respond with a 201 "Created" and a Location header containing the url of the created resource
+        UriComponents uriComponents = builder.path("/api/userState/{reference}").buildAndExpand(reference);
+        return ResponseEntity.created(uriComponents.toUri()).build();
     }
 
     @PutMapping("/{reference}")
@@ -54,5 +68,15 @@ public class UserStateController {
             .set(USER_STATE.UPDATED, Timestamp.from(Instant.now()))
             .where(USER_STATE.REFERENCE.eq(reference))
             .execute();
+    }
+
+    // TODO BEISDEAS-191 Generate 3 words as reference
+    private static String generateReference() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < REFERENCE_CHARACTER_LENGTH; i++) {
+            int character = (int) (Math.random() * ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+        }
+        return builder.toString();
     }
 }
