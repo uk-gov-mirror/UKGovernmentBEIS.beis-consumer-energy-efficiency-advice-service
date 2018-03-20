8e87746b5475dd70642d31cc846e5b795350283c
Fuad Ashanike <fuad.ashanike@softwire.com>
2018-03-20 17:33:42 +0000

    made a start on downloading pdfs

--- angular/src/app/energy-efficiency/your-plan-page/download-plan/download-plan.component.ts
+++ angular/src/app/energy-efficiency/your-plan-page/download-plan/download-plan.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {ResponseData} from '../../../shared/response-data/response-data';
 import {TenureType} from '../../../questionnaire/questions/tenure-type-question/tenure-type';
+import * as html2pdf from 'html2pdf.js';
 
 @Component({
     selector: 'app-download-plan',
@@ -12,8 +13,42 @@ export class DownloadPlanComponent {
     constructor(private responseData: ResponseData) {
     }
 
+    public onPdfClicked() {
+        const expandableElements = document.getElementsByClassName("step-main-row");
+        const elementsArray = [].slice.call(expandableElements);
+        Promise.all([this.clickElems(elementsArray)])
+            .then(() => {
+                return new Promise(resolve => {
+                    setTimeout(() => {
+                        const pdfBody = document.getElementsByClassName("your-plan-content")[0];
+                        html2pdf(pdfBody);
+                        resolve();
+                    }, 5 );
+                });
+            })
+        .then(() => {
+            this.clickElems(elementsArray);
+        });
+    }
+
     isTenant(): boolean {
         return this.responseData.tenureType !== TenureType.OwnerOccupancy &&
             !!this.responseData.tenureType;
+    }
+
+    public clickElems(elems) {
+        return elems.map(elem => {
+            return this.eventFire(elem, 'click');
+        });
+    }
+
+    public eventFire(el, etype) {
+        if (el.fireEvent) {
+            return el.fireEvent('on' + etype);
+        } else {
+            const evObj = document.createEvent('Events');
+            evObj.initEvent(etype, true, false);
+            return el.dispatchEvent(evObj);
+        }
     }
 }
