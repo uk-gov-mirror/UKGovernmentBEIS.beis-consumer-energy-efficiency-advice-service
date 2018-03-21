cad7de6a0f9ac84ea490a8a3306101ac221e0b9b
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-03-21 12:50:43 +0000

    BEISDEAS-180 Display the reference in the "need-help" component

--- angular/src/app/shared/user-state-service/user-state-service.ts
+++ angular/src/app/shared/user-state-service/user-state-service.ts
@@ -9,12 +9,21 @@ import {UserStateApiService} from "../user-state-api-service/user-state-api-serv
 export class UserStateService {
     // The user's unique session reference
     private reference: string;
+    private referencePromise: Promise<string>;
+    private resolveReference: (reference: string) => void;
 
     constructor(
         private userStateApiService: UserStateApiService,
         private location: Location,
         private router: Router,
-        private responseData: ResponseData) {}
+        private responseData: ResponseData) {
+
+        // The "need-help" component may request the reference before it has been generated
+        // We instead return a promise of the reference, that is resolved once the backend has generated it
+        this.referencePromise = new Promise(resolve => {
+            this.resolveReference = resolve;
+        });
+    }
 
     sendState(questionIndex?: number) {
         if (this.reference) {
@@ -23,6 +32,7 @@ export class UserStateService {
             this.userStateApiService.sendNewState(this.buildUserState(questionIndex))
                 .subscribe(reference => {
                     this.reference = reference;
+                    this.resolveReference(reference);
                 });
         }
     }
@@ -31,6 +41,7 @@ export class UserStateService {
         this.userStateApiService.fetchUserStateBySessionReference(reference)
             .subscribe(state => {
                 this.reference = reference;
+                this.referencePromise = Promise.resolve( this.reference);
                 replaceOldResponseData(this.responseData, state.responseData);
                 if (state.questionIndex) {
                     this.router.navigate([state.url], {queryParams: {startingQuestion: state.questionIndex}});
@@ -40,10 +51,14 @@ export class UserStateService {
             }, onError);
     }
 
+    getSessionReference() {
+        return this.referencePromise;
+    }
+
     private buildUserState(questionIndex?: number): UserState {
         const userState: UserState = {
             responseData: this.responseData,
-            // TODO BEISDEAS-180 Make this more robust
+            // Remove any query params from the current url
             url: this.location.path().split('?')[0]
         };
         if (questionIndex) {
