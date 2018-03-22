476c97f03dd82bf8c450d1e06f6496f382f9b1c6
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-03-22 11:13:06 +0000

    BEISDEAS-193 Allow the IP validation to work with IP ranges for both IPv4 and IPv6

--- user-site-server/src/main/java/uk/gov/beis/dceas/service/IpValidationService.java
+++ user-site-server/src/main/java/uk/gov/beis/dceas/service/IpValidationService.java
@@ -1,6 +1,7 @@
 package uk.gov.beis.dceas.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.web.util.IpAddressMatcher;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Takes a list of masks from config and checks whether the user is coming from one of
+ * these whitelisted IP addresses. THis will work for both IPv4 and IPv6
+ */
 @Service
 public class IpValidationService {
     private final List<String> ipWhitelist;
@@ -26,10 +31,17 @@ public class IpValidationService {
         }
 
         for (String ipAddress : ipAddresses) {
-            if (ipWhitelist.contains(ipAddress)) {
-                return true;
+            for (String subnet : ipWhitelist) {
+                if (matches(ipAddress, subnet)) {
+                    return true;
+                }
             }
         }
         return false;
+    }
+
+    private boolean matches(String ip, String subnet) {
+        IpAddressMatcher ipAddressMatcher = new IpAddressMatcher(subnet);
+        return ipAddressMatcher.matches(ip);
     }
 }
