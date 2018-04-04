89f5bb55992157da879bb034c872ed9c6d46a7ef
Unknown <raam.chauhan@softwire.com>
2018-04-04 14:00:21 +0100

    BEISDEAS-220 Create your-home module and component

---
+++ angular/src/app/your-home/your-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-your-home',
+  templateUrl: './your-home.component.html',
+  styleUrls: ['./your-home.component.scss']
+})
+export class YourHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angular/src/app/your-home/your-home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { YourHomeComponent } from './your-home.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [YourHomeComponent]
+})
+export class YourHomeModule { }
