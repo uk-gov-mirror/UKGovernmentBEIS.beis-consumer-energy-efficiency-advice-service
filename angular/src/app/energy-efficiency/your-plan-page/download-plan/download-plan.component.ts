cc8343da5f5956dbe82e9ccba4b29e0b4ab99c83
Fuad Ashanike <fuad.ashanike@softwire.com>
2018-04-09 16:56:23 +0100

    reworked download pdf

--- angular/src/app/energy-efficiency/your-plan-page/download-plan/download-plan.component.ts
+++ angular/src/app/energy-efficiency/your-plan-page/download-plan/download-plan.component.ts
@@ -15,7 +15,7 @@ export class DownloadPlanComponent {
 
     static clickElems(elems) {
         return elems.map(elem => {
-            return this.eventFire(elem, 'click');
+            elem.className += " expanded";
         });
     }
 
@@ -35,36 +35,23 @@ export class DownloadPlanComponent {
     }
 
     public onPdfClicked() {
-        const expandableElements = document.getElementsByClassName("step-details-drawer"); // Find all expandable elements
-        const elementsArray = Array.from(expandableElements); // Convert to array because they are less limited than iterables
+        // Apply a class to the whole page that tells it how to style everything
+        // Use a mutation observer to make sure everything's printed afterwards
 
-        const expandedElements = document.querySelectorAll(".expanded.step-details-drawer"); // Find all elements already expanded
-        const exElementsArray = Array.from(expandedElements);
+        const stepCards = document.getElementsByClassName("recommendation-step-card"); // Find all step cards
+        const stepCardArray = Array.from(stepCards); // Convert to array because they are less limited than iterables
 
-        const nonExElementsArray = [];
+        const reccomendationPageRow = document.getElementsByClassName("recommendations")[0];
 
-        // By filtering out elements which are both expandable and expanded, find the non-expanded elements
-        this.filterNonUnique(elementsArray, exElementsArray, nonExElementsArray);
-
-        const nonExCount = nonExElementsArray.length;
-
-        const nonExElementSiblings = [];
-        for (const elem of nonExElementsArray) {
-            // Find the sibling of the non-expanded element (Only one) and push it
-            nonExElementSiblings.push(elem.parentNode.childNodes[0].nextSibling);
-        }
+        console.log(reccomendationPageRow)
 
         let mutationCount = 0;
 
         const callback = function(observer) {
             mutationCount++;
+            console.log(mutationCount);
 
-            if (mutationCount === nonExCount) { // If all drop downs have been opened
-                // Do some styling
-                for (const elem of Array.from(<HTMLCollection>document.querySelectorAll('.headline'))) {
-                    (<HTMLElement>elem).style.marginLeft = "20px";
-                    (<HTMLElement>elem.nextElementSibling).style.marginLeft = "20px";
-                }
+            if (mutationCount === (stepCards.length + 1)) { // If all drop downs have been opened
 
                 (<HTMLElement>document.querySelector(".sticky-row")).style.visibility = "hidden";
 
@@ -73,13 +60,10 @@ export class DownloadPlanComponent {
                 html2pdf(pdfBody);
 
                 // Close all the drop downs
-                DownloadPlanComponent.clickElems(nonExElementSiblings);
-
-                // Undo the styling
-                for (const elem of Array.from(document.getElementsByClassName('.headline'))) {
-                    (<HTMLElement>elem).style.marginLeft = "0px";
-                    (<HTMLElement>elem.nextElementSibling).style.marginLeft = "0px";
-                }
+                stepCardArray.map(elem => {
+                    elem.className = elem.className.slice(0, -11);
+                });
+                reccomendationPageRow.className = reccomendationPageRow.className.slice(0, -11);
 
                 (<HTMLElement>document.querySelector(".sticky-row")).style.visibility = "visible";
 
@@ -90,18 +74,27 @@ export class DownloadPlanComponent {
 
         const observer = this.instantiateObserver(callback);
 
-        for (const elem of elementsArray) {
+        for (const elem of stepCardArray) {
             observer.observe(elem, { // Attributes which should be observed
-                attributes: true
+                attributes: true,
+                subtree: true
             });
         }
 
-        DownloadPlanComponent.clickElems(nonExElementSiblings);
+        observer.observe(reccomendationPageRow, { // Attributes which should be observed
+            attributes: true,
+            subtree: true
+        });
+
+        stepCardArray.map(elem => {
+            elem.className += " print-mode";
+        });
+        reccomendationPageRow.className += " print-mode";
     }
 
     private instantiateObserver(callback) {
         const observer = new MutationObserver((mutations) => {
-            mutations.forEach((mutation) => callback(observer));
+            mutations.forEach((mutation) => {console.log(mutation); callback(observer); });
         });
 
         return observer;
