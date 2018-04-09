f9b02100c8fbc528b0b3dacdd532e1d556747f4c
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-04-09 15:09:41 +0100

    BEISDEAS-231 Add more detail to comment

--- angular/src/app/questionnaire/questionnaire.component.ts
+++ angular/src/app/questionnaire/questionnaire.component.ts
@@ -182,8 +182,9 @@ export class QuestionnaireComponent implements OnInit, OnDestroy {
         if (!!question) {
             this.questionTypeIconClassName = QuestionTypeUtil.getIconClassName(question.questionType);
             this.currentQuestionId = question.questionId;
-            // Some properties on this.currentQuestionContent are modified, which would cause them to change in the
-            // original array. For this reason, we perform a shallow clone to maintain this.allQuestionsContent
+            // The questionHeading of this.currentQuestionContent is modified - the placeholders in the string are
+            // replaced. This modified value is used in a number of places. This change causes a change in the original
+            // array. For this reason, we perform a shallow clone to maintain this.allQuestionsContent.
             this.currentQuestionContent = Object.assign({}, this.allQuestionsContent[this.currentQuestionId]);
             if (!(this.currentQuestionContent && this.currentQuestionContent.questionHeading)) {
                 this.displayErrorAndLogMessage(`Missing question content for question with id "${ this.currentQuestionId }"`);
