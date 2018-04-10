7f1a1ffb9706b779556aabda08487a45561f6485
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-04-10 12:19:05 +0100

    BEISDEAS-148 Add logos for all suppliers
    Logos are stored in the frontend assets folder and loaded using the slug of the supplier.
    This is done to avoid wordpress related complications.

--- angular/src/app/shared/wordpress-suppliers-service/wordpress-supplier.ts
+++ angular/src/app/shared/wordpress-suppliers-service/wordpress-supplier.ts
@@ -1,8 +1,12 @@
 export class WordpressSupplier {
+    // The slug is used to fetch the logo for the supplier from
+    // /assets/images/eco-suppliers/{{slug}}.jpeg
+    slug: string;
     name: string;
     link: string;
 
     constructor(rawSupplier: RawWordpressSupplier) {
+        this.slug = rawSupplier.slug;
         this.name = rawSupplier.acf.name;
         this.link = rawSupplier.acf.supplier_info_link;
     }
