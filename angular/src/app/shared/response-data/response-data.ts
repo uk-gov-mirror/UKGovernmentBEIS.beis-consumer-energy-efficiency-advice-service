c496f06d4ee31995713db1c75de4d6abf628cff1
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-04-18 15:10:59 +0100

    BEISDEAS-221 Simplify local storage detection

--- angular/src/app/shared/response-data/response-data.ts
+++ angular/src/app/shared/response-data/response-data.ts
@@ -110,6 +110,7 @@ export class ResponseData {
 
 const responseDataSessionStorageKey = 'responseData';
 
+// Simplified version of https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Feature-detecting_localStorage
 function sessionStorageAvailable() {
     try {
         const x = '__storage_test__';
@@ -117,18 +118,7 @@ function sessionStorageAvailable() {
         sessionStorage.removeItem(x);
         return true;
     } catch (e) {
-        return e instanceof DOMException && (
-            // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            sessionStorage.length !== 0;
+        return false;
     }
 }
 
