fd577e1cdbffa031ee24b3f849bebb9e4f573019
Raam Chauhan <raam.chauhan@softwire.com>
2018-04-20 11:17:33 +0100

    BEISDEAS-248 Add caching when getting articles

--- angular/src/app/shared/wordpress-pages-service/wordpress-pages.service.ts
+++ angular/src/app/shared/wordpress-pages-service/wordpress-pages.service.ts
@@ -12,10 +12,11 @@ export class WordpressPagesService {
     private static readonly pagesEndpoint = 'wp/v2/pages';
 
     private topLevelPages: Observable<WordpressPage[]>;
-    private latestPages: Observable<WordpressPage[]>;
+    private latestPagesByTag: {[tag: string]: Observable<WordpressPage[]>};
     private pages: {[slug: string]: Observable<ExtendedWordpressPage>} = {};
 
     constructor(private http: HttpClient, private wordpressApiService: WordpressApiService) {
+        this.latestPagesByTag = {};
     }
 
     /**
@@ -46,7 +47,7 @@ export class WordpressPagesService {
     }
 
     getLatestPages(tag: string): Observable<WordpressPage[]> {
-        if (!this.latestPages) {
+        if (!this.latestPagesByTag[tag]) {
             const params = new HttpParams()
                 .set('tag', tag)
                 .set('per_page', '4')
@@ -54,11 +55,11 @@ export class WordpressPagesService {
                 .set('order', 'desc')
                 .set('context', 'embed');
             const endpoint = this.wordpressApiService.getFullApiEndpoint(WordpressPagesService.pagesEndpoint);
-            this.latestPages = this.http.get(endpoint, {params: params})
+            return this.latestPagesByTag[tag] = this.http.get(endpoint, {params: params})
                 .map((pageResponses: WordpressPageResponse[]) => pageResponses.map(pageResponse => new WordpressPage(pageResponse)))
                 .shareReplay(1);
         }
-        return this.latestPages;
+        return this.latestPagesByTag[tag];
     }
 
     getPage(slug: string): Observable<ExtendedWordpressPage> {
