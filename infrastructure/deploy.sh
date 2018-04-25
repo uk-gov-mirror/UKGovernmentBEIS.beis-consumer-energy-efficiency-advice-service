b3d5c08abb0fd6b9b9a4241178282321fbf5c3a5
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-04-25 16:40:10 +0100

    BEISDEAS-242 Tidy up deployment scripts and only trigger int deployment on commits to master

---
+++ infrastructure/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ex
+
+# This is the CI server script to deploy the user-site and admin-site
+#
+# The working directory should be the git root
+#
+# The caller should have the following environment variables set:
+#
+# USERNAME: cloudfoundry username
+# PASSWORD: cloudfoundry password
+# SPACE: the space to which you want to deploy
+
+cf --version
+cf login -a api.cloud.service.gov.uk -u $USERNAME -p $PASSWORD -o "beis-domestic-energy-advice-service" -s $SPACE
+./ci-admin-site-deploy.sh
+./ci-user-site-deploy.sh
+cf logout
