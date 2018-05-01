1f42b42a77733b0a870db60f60a13fbbe459b288
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-05-01 15:59:26 +0100

    BEISDEAS-294 Change copy for eco-suppliers
    If the user has not completed a survey, then show an extra bit of text

--- angular/src/app/eco-suppliers-page/eco-suppliers-page.component.spec.ts
+++ angular/src/app/eco-suppliers-page/eco-suppliers-page.component.spec.ts
@@ -5,10 +5,12 @@ import {By} from '@angular/platform-browser';
 import { ECOSuppliersPageComponent } from './eco-suppliers-page.component';
 import {WordpressECOSuppliersService} from "../shared/wordpress-eco-suppliers-service/wordpress-eco-suppliers.service";
 import {SpinnerAndErrorContainerComponent} from "../shared/spinner-and-error-container/spinner-and-error-container.component";
+import {ResponseData} from "../shared/response-data/response-data";
 
 describe('ECOSuppliersPageComponent', () => {
     let component: ECOSuppliersPageComponent;
     let fixture: ComponentFixture<ECOSuppliersPageComponent>;
+    const responseData: ResponseData = new ResponseData();
     const expectedSuppliers = [{
         name: 'British Gas',
         link: 'https://britishgas.co.uk'
@@ -24,7 +26,8 @@ describe('ECOSuppliersPageComponent', () => {
                 SpinnerAndErrorContainerComponent,
             ],
             providers: [
-                {provide: WordpressECOSuppliersService, useValue: wordpressECOSuppliersServiceStub}
+                {provide: WordpressECOSuppliersService, useValue: wordpressECOSuppliersServiceStub},
+                {provide: ResponseData, useClass: responseData},
             ]
         })
         .compileComponents();
