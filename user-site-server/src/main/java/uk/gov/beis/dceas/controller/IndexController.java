66ccb4ea5388fe1ef1168f4558559ab8c56e7189
Richard Bradley <Richard.Bradley@softwire.com>
2018-05-22 13:20:22 +0000

    BEISDEAS-338 add TODOs

--- user-site-server/src/main/java/uk/gov/beis/dceas/controller/IndexController.java
+++ user-site-server/src/main/java/uk/gov/beis/dceas/controller/IndexController.java
@@ -146,6 +146,8 @@ public class IndexController {
         model.addAttribute("buildNumber",
             buildAttributes.getValue("Build-Number"));
 
+        // TODO:BEISDEAS-338 request.getServerName() will not work, due to CDN and
+        // CloudFoundry router layers. What is this trying to do?
         model.addAttribute("host", request.getServerName());
 
         return "index";
