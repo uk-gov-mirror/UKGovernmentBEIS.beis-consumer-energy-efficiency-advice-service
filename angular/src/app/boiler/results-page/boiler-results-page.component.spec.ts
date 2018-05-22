84bb3901f5d29b384dded003d346e989b7c403f0
Raam Chauhan <raam.chauhan@softwire.com>
2018-05-22 16:31:31 +0100

    BEISDEAS-324 Fix tests for boiler results page

--- angular/src/app/boiler/results-page/boiler-results-page.component.spec.ts
+++ angular/src/app/boiler/results-page/boiler-results-page.component.spec.ts
@@ -23,11 +23,6 @@ describe('BoilerResultsPageComponent', () => {
     let component: BoilerResultsPageComponent;
     let fixture: ComponentFixture<BoilerResultsPageComponent>;
 
-    const boilerPageMeasures = require('assets/test/boiler-page-measures.json');
-    const boilerPageMeasuresServiceStub = {
-        fetchMeasuresForBoilerPages: () => Observable.of(boilerPageMeasures)
-    };
-
     const boilerTypesResponse = require('assets/test/boiler-types-response.json');
     const boilerTypesServiceStub = {
         fetchBoilerTypes: () => Observable.of(boilerTypesResponse)
@@ -59,7 +54,6 @@ describe('BoilerResultsPageComponent', () => {
             ],
             providers: [
                 ResponseData,
-                {provide: BoilerPageMeasuresService, useValue: boilerPageMeasuresServiceStub},
                 {provide: BoilerTypesService, useValue: boilerTypesServiceStub},
                 {provide: QuestionnaireService, useValue: questionnaireServiceStub},
                 {provide: UserStateService, useValue: userStateServiceStub},
@@ -97,15 +91,6 @@ describe('BoilerResultsPageComponent', () => {
         boilerTypesServiceStub.fetchBoilerTypes().toPromise().then(expectedBoilers => {
             expect(actualBoilers.length).toBe(expectedBoilers.length);
             expectedBoilers.forEach(boiler => expect(actualBoilers).toContain(boiler));
-        });
-    });
-
-    it('should show energy saving measures', () => {
-        const measuresSection = fixture.debugElement.query(By.directive(BoilerMeasuresSectionComponent));
-        const actualMeasures = measuresSection.componentInstance.measures;
-
-        boilerPageMeasuresServiceStub.fetchMeasuresForBoilerPages().toPromise().then(expectedMeasures => {
-            expect(actualMeasures).toEqual(expectedMeasures);
         });
     });
 });
