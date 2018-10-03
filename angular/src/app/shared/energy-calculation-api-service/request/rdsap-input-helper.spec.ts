b88de7991c0815808e46c73189f029d06e5d9c50
Bob Chen <Bob.Chen@softwire.com>
2018-10-03 18:40:00 +0100

    ESASSUP-22 - Add more unit tests

--- angular/src/app/shared/energy-calculation-api-service/request/rdsap-input-helper.spec.ts
+++ angular/src/app/shared/energy-calculation-api-service/request/rdsap-input-helper.spec.ts
@@ -6,6 +6,7 @@ import {ResponseData} from "../../response-data/response-data";
 import {HouseExposedWall} from "../../../questionnaire/questions/house-exposed-wall-question/house-exposed-wall";
 import {FlatExposedWall} from "../../../questionnaire/questions/flat-exposed-wall-question/flat-exposed-wall";
 import {BuiltForm} from "./built-form";
+import {PropertyType} from "./property-type";
 
 describe('RdsapInputHelper', () => {
 
@@ -85,6 +86,17 @@ describe('RdsapInputHelper', () => {
             responseData = {} as ResponseData;
         });
 
+        it('should return detached for detached house', () => {
+           // given
+            responseData.homeType = HomeType.DetachedHouse;
+
+            // when
+            const builtForm = RdsapInputHelper.getBuiltForm(responseData);
+
+            // then
+            expect(builtForm).toEqual(BuiltForm.Detached);
+        });
+
         it('should return semi-detached for semi-detached house sharing 1 side', () => {
             // given
             responseData.homeType = HomeType.SemiDetachedOrTerracedHouse;
@@ -119,6 +131,28 @@ describe('RdsapInputHelper', () => {
 
             // then
             expect(builtForm).toEqual(BuiltForm.EnclosedMidTerrace);
+        });
+
+        it('should return detached for detached bungalow', () => {
+            // given
+            responseData.homeType = HomeType.DetachedBungalow;
+
+            // when
+            const builtForm = RdsapInputHelper.getBuiltForm(responseData);
+
+            // then
+            expect(builtForm).toEqual(BuiltForm.Detached);
+        });
+
+        it('should return semi-detached for semi-detached bungalow', () => {
+            // given
+            responseData.homeType = HomeType.SemiDetachedBungalow;
+
+            // when
+            const builtForm = RdsapInputHelper.getBuiltForm(responseData);
+
+            // then
+            expect(builtForm).toEqual(BuiltForm.SemiDetached);
         });
 
         it('should return detached for flat sharing 0 sides', () => {
@@ -179,6 +213,91 @@ describe('RdsapInputHelper', () => {
 
             // then
             expect(builtForm).toEqual(BuiltForm.EnclosedMidTerrace);
+        });
+
+        it('should return detached for park home or mobile home', () => {
+            // given
+            responseData.homeType = HomeType.ParkHomeOrMobileHome;
+
+            // when
+            const builtForm = RdsapInputHelper.getBuiltForm(responseData);
+
+            // then
+            expect(builtForm).toEqual(BuiltForm.Detached);
+        });
+    });
+
+    describe('#getPropertyType', () => {
+        let responseData: ResponseData;
+
+        beforeEach(() => {
+            responseData = {} as ResponseData;
+        });
+
+        it('should return house for detached house', () => {
+            // given
+            const homeType = HomeType.DetachedHouse;
+
+            // when
+            const propertyType = RdsapInputHelper.getPropertyType(homeType);
+
+            // then
+            expect(propertyType).toEqual(PropertyType.House);
+        });
+
+        it('should return house for semi-detached or terraced house', () => {
+            // given
+            const homeType = HomeType.SemiDetachedOrTerracedHouse;
+
+            // when
+            const propertyType = RdsapInputHelper.getPropertyType(homeType);
+
+            // then
+            expect(propertyType).toEqual(PropertyType.House);
+        });
+
+        it('should return bungalow for detached bungalow', () => {
+            // given
+            const homeType = HomeType.DetachedBungalow;
+
+            // when
+            const propertyType = RdsapInputHelper.getPropertyType(homeType);
+
+            // then
+            expect(propertyType).toEqual(PropertyType.Bungalow);
+        });
+
+        it('should return bungalow for semi-detached bungalow', () => {
+            // given
+            const homeType = HomeType.SemiDetachedBungalow;
+
+            // when
+            const propertyType = RdsapInputHelper.getPropertyType(homeType);
+
+            // then
+            expect(propertyType).toEqual(PropertyType.Bungalow);
+        });
+
+        it('should return flat for flat, duplex or maisonette', () => {
+            // given
+            const homeType = HomeType.FlatDuplexOrMaisonette;
+
+            // when
+            const propertyType = RdsapInputHelper.getPropertyType(homeType);
+
+            // then
+            expect(propertyType).toEqual(PropertyType.Flat);
+        });
+
+        it('should return park home for park home or mobile home', () => {
+            // given
+            const homeType = HomeType.ParkHomeOrMobileHome;
+
+            // when
+            const propertyType = RdsapInputHelper.getPropertyType(homeType);
+
+            // then
+            expect(propertyType).toEqual(PropertyType.ParkHome);
         });
     });
 });
