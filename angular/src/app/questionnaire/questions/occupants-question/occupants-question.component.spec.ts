6c941842b7b2f90822e9f0b6893ab526241cff76
Bob Chen <Bob.Chen@softwire.com>
2018-11-19 18:08:03 +0000

    ESASSUP-63 - Make tests more robust

--- angular/src/app/questionnaire/questions/occupants-question/occupants-question.component.spec.ts
+++ angular/src/app/questionnaire/questions/occupants-question/occupants-question.component.spec.ts
@@ -13,8 +13,17 @@ describe('OccupantsQuestionComponent', () => {
     let component: OccupantsQuestionComponent;
     let fixture: ComponentFixture<OccupantsQuestionComponent>;
 
-    const ORIGINAL_VALUE: number = 1;
-    const NEW_VALUE: number = 2;
+    const originalNumberOfAdultsUnder64: number = 1;
+    const originalNumberOfAdults64To80: number = 2;
+    const originalNumberOfAdultsOver80: number = 3;
+    const originalNumberOfChildrenAged5AndAbove: number = 4;
+    const originalNumberOfChildrenUnder5: number = 5;
+
+    const newNumberOfAdultsUnder64: number = 11;
+    const newNumberOfAdults64To80: number = 12;
+    const newNumberOfAdultsOver80: number = 13;
+    const newNumberOfChildrenAged5AndAbove: number = 14;
+    const newNumberOfChildrenUnder5: number = 15;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -28,11 +37,11 @@ describe('OccupantsQuestionComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(OccupantsQuestionComponent);
         component = fixture.componentInstance;
-        component.adultsAgedUnder64 = ORIGINAL_VALUE;
-        component.adultsAged64To80 = ORIGINAL_VALUE;
-        component.adultsAgedOver80 = ORIGINAL_VALUE;
-        component.childrenAged5AndAbove = ORIGINAL_VALUE;
-        component.childrenAgedUnder5 = ORIGINAL_VALUE;
+        component.adultsAgedUnder64 = originalNumberOfAdultsUnder64;
+        component.adultsAged64To80 = originalNumberOfAdults64To80;
+        component.adultsAgedOver80 = originalNumberOfAdultsOver80;
+        component.childrenAged5AndAbove = originalNumberOfChildrenAged5AndAbove;
+        component.childrenAgedUnder5 = originalNumberOfChildrenUnder5;
         fixture.detectChanges();
     });
 
@@ -43,100 +52,85 @@ describe('OccupantsQuestionComponent', () => {
     it('should populate with original number of adults under 64 in response data', async(() => {
         fixture.whenStable().then(() => {
             const adultsUnder64Input = fixture.debugElement.query(By.css('.adults-under-64-input input'));
-            expect(adultsUnder64Input.nativeElement.value).toBe(ORIGINAL_VALUE.toString());
+            expect(adultsUnder64Input.nativeElement.value).toBe(originalNumberOfAdultsUnder64.toString());
         });
     }));
 
     it('should set the response given a valid number of adults aged under 64', () => {
-        // given
-        const expectedAdultsUnder64 = NEW_VALUE;
-
         // when
         const adultsInput = fixture.debugElement.query(By.css('.adults-under-64-input input'));
-        adultsInput.nativeElement.value = expectedAdultsUnder64;
+        adultsInput.nativeElement.value = newNumberOfAdultsUnder64;
         adultsInput.nativeElement.dispatchEvent(new Event('input'));
 
         // then
-        expect(component.adultsAgedUnder64).toBe(expectedAdultsUnder64);
+        expect(component.adultsAgedUnder64).toBe(newNumberOfAdultsUnder64);
     });
 
     it('should populate with original number of adults aged 64 to 80 in response data', async(() => {
         fixture.whenStable().then(() => {
             const adults64To80Input = fixture.debugElement.query(By.css('.adults-64-to-80-input input'));
-            expect(adults64To80Input.nativeElement.value).toBe(ORIGINAL_VALUE.toString());
+            expect(adults64To80Input.nativeElement.value).toBe(originalNumberOfAdults64To80.toString());
         });
     }));
 
     it('should set the response given a valid number of adults aged 64 to 80', () => {
-        // given
-        const expectedAdults64To80 = NEW_VALUE;
-
         // when
         const adultsInput = fixture.debugElement.query(By.css('.adults-64-to-80-input input'));
-        adultsInput.nativeElement.value = expectedAdults64To80;
+        adultsInput.nativeElement.value = newNumberOfAdults64To80;
         adultsInput.nativeElement.dispatchEvent(new Event('input'));
 
         // then
-        expect(component.adultsAged64To80).toBe(expectedAdults64To80);
+        expect(component.adultsAged64To80).toBe(newNumberOfAdults64To80);
     });
 
     it('should populate with original number of adults over 80 in response data', async(() => {
         fixture.whenStable().then(() => {
             const adultsOver80Input = fixture.debugElement.query(By.css('.adults-over-80-input input'));
-            expect(adultsOver80Input.nativeElement.value).toBe(ORIGINAL_VALUE.toString());
+            expect(adultsOver80Input.nativeElement.value).toBe(originalNumberOfAdultsOver80.toString());
         });
     }));
 
     it('should set the response given a valid number of adults aged 64 to 80', () => {
-        // given
-        const expectedAdultsOver80 = NEW_VALUE;
-
         // when
         const adultsInput = fixture.debugElement.query(By.css('.adults-over-80-input input'));
-        adultsInput.nativeElement.value = expectedAdultsOver80;
+        adultsInput.nativeElement.value = newNumberOfAdultsOver80;
         adultsInput.nativeElement.dispatchEvent(new Event('input'));
 
         // then
-        expect(component.adultsAgedOver80).toBe(expectedAdultsOver80);
+        expect(component.adultsAgedOver80).toBe(newNumberOfAdultsOver80);
     });
 
     it('should populate with original number of children aged 5 and above in response data', async(() => {
         fixture.whenStable().then(() => {
             const childrenInput = fixture.debugElement.query(By.css('.children-aged-5-and-above-input input'));
-            expect(childrenInput.nativeElement.value).toBe(ORIGINAL_VALUE.toString());
+            expect(childrenInput.nativeElement.value).toBe(originalNumberOfChildrenAged5AndAbove.toString());
         });
     }));
 
     it('should set the response given a valid number of children aged 5 and above', () => {
-        // given
-        const expectedChildrenAged5AndAbove = NEW_VALUE;
-
         // when
         const childrenInput = fixture.debugElement.query(By.css('.children-aged-5-and-above-input input'));
-        childrenInput.nativeElement.value = expectedChildrenAged5AndAbove;
+        childrenInput.nativeElement.value = newNumberOfChildrenAged5AndAbove;
         childrenInput.nativeElement.dispatchEvent(new Event('input'));
 
         // then
-        expect(component.childrenAged5AndAbove).toBe(expectedChildrenAged5AndAbove);
+        expect(component.childrenAged5AndAbove).toBe(newNumberOfChildrenAged5AndAbove);
     });
 
     it('should populate with original number of children under 5 in response data', async(() => {
         fixture.whenStable().then(() => {
             const childrenInput = fixture.debugElement.query(By.css('.children-under-5-input input'));
-            expect(childrenInput.nativeElement.value).toBe(ORIGINAL_VALUE.toString());
+            expect(childrenInput.nativeElement.value).toBe(originalNumberOfChildrenUnder5.toString());
         });
     }));
 
     it('should set the response given a valid number of children aged 5 and above', () => {
-        // given
-        const expectedChildrenUnder5 = NEW_VALUE;
-
         // when
         const childrenInput = fixture.debugElement.query(By.css('.children-under-5-input input'));
-        childrenInput.nativeElement.value = expectedChildrenUnder5;
+        childrenInput.nativeElement.value = newNumberOfChildrenUnder5;
         childrenInput.nativeElement.dispatchEvent(new Event('input'));
 
         // then
-        expect(component.childrenAgedUnder5).toBe(expectedChildrenUnder5);
+        expect(component.childrenAgedUnder5).toBe(newNumberOfChildrenUnder5);
     });
 });
