6aee1fd4edcb0464a1c46651a0cbfbffb7cf9f52
Bob Chen <Bob.Chen@softwire.com>
2019-01-11 17:21:40 +0000

    ESASSUP-48 - Fix a bug that the benefits questions still show up when the user has already clicked 'no' to grant questionnaire question

--- angular/src/app/questionnaire/questions/benefits-questions/societal-benefits-question-metadata.ts
+++ angular/src/app/questionnaire/questions/benefits-questions/societal-benefits-question-metadata.ts
@@ -14,7 +14,8 @@ export class SocietalBenefitsQuestionMetadata extends QuestionMetadata {
 
     // If you change question order, the other isApplicable functions in benefits-questions should be revised
     isApplicable(responseData: ResponseData): boolean {
-        return !responseData.receivePensionGuaranteeCredit
+        return responseData.shouldIncludeGrantsQuestionnaire
+            && !responseData.receivePensionGuaranteeCredit
             && !responseData.receiveIncomeRelatedBenefits;
     }
 
