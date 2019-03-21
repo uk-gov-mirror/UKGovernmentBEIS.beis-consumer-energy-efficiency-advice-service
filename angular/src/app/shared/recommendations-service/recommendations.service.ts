c33f1d5182cde93568df1f8ce25ec52a4fee830a
Bob Chen <Bob.Chen@softwire.com>
2019-03-21 09:03:16 +0000

    ESASSUP-100 - Make getMeasuresFromEnergyCalculation not return null

--- angular/src/app/shared/recommendations-service/recommendations.service.ts
+++ angular/src/app/shared/recommendations-service/recommendations.service.ts
@@ -112,7 +112,7 @@ export class RecommendationsService {
 
     private getMeasuresFromEnergyCalculation(energyCalculation: EnergyCalculationResponse): MeasuresResponse<EnergySavingMeasureResponse> {
         if (this.responseData.tenureType !== TenureType.OwnerOccupancy && !!this.responseData.tenureType) {
-            return energyCalculation.measures_rented || energyCalculation.default_rental_measures;
+            return energyCalculation.measures_rented || energyCalculation.default_rental_measures || {};
         }
         return energyCalculation.measures;
     }
