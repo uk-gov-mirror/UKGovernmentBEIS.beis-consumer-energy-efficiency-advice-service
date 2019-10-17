10d24d66d6a5afeb6a8e67b2cf04f6001b22fb13
Jamie Humphries <jamie.humphries@softwire.com>
2019-10-17 12:15:03 +0100

    Fixes to local authority user creation.

--- wordpress/wp-content/themes/angular-theme/functions/helpers/create_user_for_local_authority.php
+++ wordpress/wp-content/themes/angular-theme/functions/helpers/create_user_for_local_authority.php
@@ -3,8 +3,8 @@
 function create_user_for_local_authority($post) {
 
     $post_ID = $post->ID;
-    $login = $post->post_name; // Lower snake case, e.g. "brigthon_and_hove".
-    $display_name = get_field('display_name', $post_ID); // With spaces and capitalisation, e.g. "Brighton and Hove".
+    $display_name = $post->post_title; // With spaces and capitalisation, e.g. "Brighton and Hove".
+    $login = sanitize_title_with_dashes($display_name); // Lower case with dashes, e.g. "brigthon-and-hove".
     $user_ID = wp_insert_user(array(
         'user_login'    => $login,
         'user_pass'     => wp_generate_password(),
@@ -15,11 +15,12 @@ function create_user_for_local_authority($post) {
         'admin_color'   => 'blue'
     ));
     if (!is_integer($user_ID)) {
+        // The user may already exist.
         $user = get_user_by('login', $login);
         if ($user) {
             $user_ID = $user->ID;
         } else {
-            return false;
+            wp_die('Failed to create or find local authority user for new local authority.');
         }
     }
     wp_update_post(array(
