47c4f78650e7c9e149a1b423e8685bb33ef0b01a
Jamie Humphries <jamie.humphries@softwire.com>
2020-03-18 14:09:34 +0000

    Conditionally change tense of MEES questions.

--- angular/src/app/questionnaire/question-heading-processor.service.ts
+++ angular/src/app/questionnaire/question-heading-processor.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {ResponseData} from '../shared/response-data/response-data';
 import {getFuelTypeDescription} from './questions/fuel-type-question/fuel-type';
 import {getHomePropertyDescription} from "../shared/home-property-description-helper/home-property-description-helper";
+import {LettingDomesticPropertyStage} from "./questions/mees/letting-domestic-property-question/letting-domestic-property-stage";
 
 export interface ReplacementProcedures {
     placeholder: RegExp;
@@ -12,6 +13,7 @@ export interface ReplacementProcedures {
 export class QuestionHeadingProcessor {
     private static readonly FUEL_TYPE_PLACEHOLDER = '{{fuel_type}}';
     private static readonly PROPERTY_PLACEHOLDER = '{{property}}';
+    private static readonly TENANCY_ARE_YOU_OR_WILL_YOU_BE_PLACEHOLDER = '{{tenancy_are_you_or_will_you_be}}';
 
     private static readonly REPLACEMENT_PROCEDURES: ReplacementProcedures[] =
         [
@@ -25,6 +27,13 @@ export class QuestionHeadingProcessor {
                 placeholder: new RegExp(QuestionHeadingProcessor.PROPERTY_PLACEHOLDER, 'g'),
                 replacementFinder: (responseData: ResponseData): string => {
                     return getHomePropertyDescription(responseData.homeType, responseData.builtForm);
+                }
+            },
+            {
+                placeholder: new RegExp(QuestionHeadingProcessor.TENANCY_ARE_YOU_OR_WILL_YOU_BE_PLACEHOLDER, 'g'),
+                replacementFinder: (responseData: ResponseData): string => {
+                    const stage = responseData.lettingDomesticPropertyStage;
+                    return stage === LettingDomesticPropertyStage.Soon ? 'will you be' : 'are you';
                 }
             }
         ];
