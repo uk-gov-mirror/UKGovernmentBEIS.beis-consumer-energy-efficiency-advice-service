18b60b14688dd77805d224573095cbd4a8a2e382
Lucas Nogueira <lucas.nogueira@softwire.com>
2020-07-14 11:29:38 +0100

    SEA-20: Add error handling to cache access

--- user-site-server/src/main/java/uk/gov/beis/dceas/controller/InstallerSearchController.java
+++ user-site-server/src/main/java/uk/gov/beis/dceas/controller/InstallerSearchController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import com.google.common.cache.CacheBuilder;
@@ -23,6 +24,7 @@ import com.google.common.cache.LoadingCache;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 
@@ -35,7 +37,10 @@ public class InstallerSearchController {
     private final String clientId;
     private final String clientSecret;
     private final RestTemplate restTemplate;
-    private final LoadingCache<String, String> accessTokenCache = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.HOURS).build(getAccessToken());
+    private final LoadingCache<String, String> accessTokenCache = CacheBuilder
+            .newBuilder()
+            .expireAfterWrite(55, TimeUnit.MINUTES)
+            .build(getAccessToken());
 
     public InstallerSearchController(
             @Value("${vcap.services.trustMark.search.url}")
@@ -59,20 +64,26 @@ public class InstallerSearchController {
             @PathVariable String postcode,
             @RequestParam String[] tradecodes,
             @RequestParam("page") Integer page) throws URISyntaxException {
-        String accessToken = accessTokenCache.getUnchecked("Trustmark Access Token");
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + accessToken);
-        HttpEntity<?> entity = new HttpEntity<>(headers);
-        // We need to wrap in new URI() rather than using the string to prevent double encoding of spaces in the
-        // postcode. Without this, postcode parameters come out as (e.g.) NW5%25201TL instead of NW5%201TL.
-        URI url = new URI(UriComponentsBuilder.fromHttpUrl(searchUrl)
-                .queryParam("postcode", postcode)
-                .queryParam("tradecodes", tradecodes)
-                .queryParam("pageNumber", page)
-                .queryParam("pageSize", numberOfItemsPerPage)
-                .toUriString());
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        return response.getBody();
+        try {
+            String accessToken = accessTokenCache.get("Trustmark Access Token");
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + accessToken);
+            HttpEntity<?> entity = new HttpEntity<>(headers);
+            // We need to wrap in new URI() rather than using the string to prevent double encoding of spaces in the
+            // postcode. Without this, postcode parameters come out as (e.g.) NW5%25201TL instead of NW5%201TL.
+            URI url = new URI(UriComponentsBuilder.fromHttpUrl(searchUrl)
+                    .queryParam("postcode", postcode)
+                    .queryParam("tradecodes", tradecodes)
+                    .queryParam("pageNumber", page)
+                    .queryParam("pageSize", numberOfItemsPerPage)
+                    .toUriString());
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+            return response.getBody();
+        } catch (ExecutionException e) {
+            return "{\"errorMessage\": \"Failed to fetch Trustmark access token from cache.\"}";
+        } catch (HttpClientErrorException e) {
+            return "{\"errorMessage\": \"Failed to fetch data from the Trustmark API.\"}";
+        }
     }
 
     private CacheLoader<String, String> getAccessToken() {
