e1352654ac30dd710dfaec8ef3077ed9ef39a796
Lucas Nogueira <lucas.nogueira@softwire.com>
2020-07-27 11:52:00 +0100

    SEA-40: Add "GHG Primary" and "GHG Secondary" tags to recommendations.

--- wordpress/wp-content/themes/angular-theme/functions/initialise_measure_post_type.php
+++ wordpress/wp-content/themes/angular-theme/functions/initialise_measure_post_type.php
@@ -220,6 +220,8 @@ function setup_measure_acf_group() {
                         'tag_make_home_warmer' => 'Make your home warmer',
                         'tag_make_home_greener' => 'Make your home greener',
                         'tag_home_improvements' => 'Home improvements',
+                        'tag_ghg_primary' => 'GHG primary',
+                        'tag_ghg_secondary' => 'GHG secondary'
                     ),
                     'default_value' => '',
                     'layout' => 'vertical',
