299af54d3022cbf392dd17dbccd0375a7a12c117
Ben Ward <ben.ward@softwire.com>
2020-08-03 11:39:38 +0100

    SEA-67 Add basic auth to all user site endpoints

---
+++ user-site-server/src/main/java/uk/gov/beis/dceas/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package uk.gov.beis.dceas.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${vcap.services.user-site-auth.credentials.username}")
+    private String username;
+    @Value("${vcap.services.user-site-auth.credentials.password}")
+    private String password;
+    @Value("${info.environment}")
+    private String environment;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (!environment.toUpperCase().equals("LIVE")) {
+            http.authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .httpBasic()
+                    .realmName("BEIS SEA");
+        }
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.inMemoryAuthentication()
+       .withUser(username).password(password).authorities("ROLE_USER");
+    }
+}
