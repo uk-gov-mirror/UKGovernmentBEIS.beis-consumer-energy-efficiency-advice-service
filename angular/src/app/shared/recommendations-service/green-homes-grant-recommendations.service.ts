fcd752cafcb9c6f424a32ab56c40fcaaad0d005a
Jamie Humphries <jamie.humphries@softwire.com>
2020-08-10 12:11:47 +0100

    SEA-38: Generalise all Green Homes Grant recommendations in service.

--- angular/src/app/shared/recommendations-service/green-homes-grant-recommendations.service.ts
+++ angular/src/app/shared/recommendations-service/green-homes-grant-recommendations.service.ts
@@ -4,31 +4,31 @@ import {EnergySavingMeasureResponse} from "../energy-calculation-api-service/res
 import {ResponseData} from "../response-data/response-data";
 import {GreenHomesGrantService} from "../../green-homes-grant/green-homes-grant-service/green-homes-grant.service";
 import {GreenHomesGrantEligibility} from "../../green-homes-grant/green-homes-grant-service/green-homes-grant-eligibility";
-import {BoilerTypesService} from "../../boiler/boiler-types-service/boiler-types.service";
 import {
     shouldRecommendAirSourceHeatPump,
     shouldRecommendGroundSourceHeatPump
 } from "../heating-systems/heating-systems";
-import {BoilerType} from "../../boiler/boiler-types-service/boiler-type";
 import {HomeType} from "../../questionnaire/questions/home-type-question/home-type";
 import {RoofType} from "../../questionnaire/questions/construction-question/construction-types";
+import {EnergySavingMeasureContentService} from "../energy-saving-measure-content-service/energy-saving-measure-content.service";
+import {MeasureContent} from "../energy-saving-measure-content-service/measure-content";
 
 export interface GreenHomesGrantRecommendation {
     code: string;
     response: EnergySavingMeasureResponse;
 }
 
-const CODES = {
-    AIR_SOURCE_HEAT_PUMP: 'Z1',
-    GROUND_SOURCE_HEAT_PUMP: 'Z4',
-    PARK_HOME_INSULATION: 'park_home_insulation', // There is no proper BRE measure for park home insulation.
-    FLAT_ROOF_INSULATION: 'A2',
-    ROOM_IN_ROOF_INSULATION: 'A3'
-};
-
-const BOILER_SLUGS = {
-    AIR_SOURCE_HEAT_PUMP: 'air-source-heat-pump',
-    GROUND_SOURCE_HEAT_PUMP: 'ground-source-heat-pump'
+const MEASURE_RECOMMENDATION_PREDICATES: { [code: string]: (responseData: ResponseData) => boolean } = {
+    // Air source heat pump.
+    'Z1': shouldRecommendAirSourceHeatPump,
+    // Ground source heat pump.
+    'Z4': shouldRecommendGroundSourceHeatPump,
+    // Park home insulation.
+    'park_home_insulation': data => data.homeType === HomeType.ParkHomeOrMobileHome,
+    // Flat roof insulation.
+    'A2': data => data.homeType !== HomeType.ParkHomeOrMobileHome && data.roofType === RoofType.FlatNoInsulation,
+    // Room-in-roof insulation.
+    'A3': data => data.homeType !== HomeType.ParkHomeOrMobileHome && data.roofType === RoofType.PitchedNoInsulation
 };
 
 @Injectable()
@@ -36,83 +36,50 @@ export class GreenHomesGrantRecommendationsService {
 
     constructor(private responseData: ResponseData,
                 private greenHomesGrantService: GreenHomesGrantService,
-                private boilerTypesService: BoilerTypesService) {
+                private measureService: EnergySavingMeasureContentService) {
     }
 
     getGreenHomesGrantRecommendations(): Observable<GreenHomesGrantRecommendation[]> {
         return Observable.forkJoin(
             this.greenHomesGrantService.getEligibility(),
-            this.boilerTypesService.fetchBoilerTypes()
-        ).mergeMap(([eligibility, boilerTypes]) => {
+            this.measureService.fetchMeasureDetails()
+        ).mergeMap(([eligibility, measuresContent]) => {
             if (eligibility === GreenHomesGrantEligibility.Ineligible) {
                 return Observable.of([]);
             }
-            const heatingSystemRecommendations = this.getHeatingSystemRecommendations(boilerTypes);
-            const insulationRecommendations = this.getInsulationRecommendations();
-            const recommendations = heatingSystemRecommendations.concat(insulationRecommendations);
+            const recommendations = [];
+            for (const [code, shouldRecommendMeasure] of Object.entries(MEASURE_RECOMMENDATION_PREDICATES)) {
+                if (!shouldRecommendMeasure(this.responseData)) {
+                    continue;
+                }
+                const recommendation = GreenHomesGrantRecommendationsService.buildRecommendation(code, measuresContent);
+                if (recommendation !== null) {
+                    recommendations.push(recommendation);
+                }
+            }
             return Observable.of(recommendations);
         });
     }
 
-    private getHeatingSystemRecommendations(boilerTypes: BoilerType[]): GreenHomesGrantRecommendation[] {
-        const recommendations: GreenHomesGrantRecommendation[] = [];
-        if (shouldRecommendAirSourceHeatPump(this.responseData)) {
-            const response = this.getBoilerTypeResponse(boilerTypes, BOILER_SLUGS.AIR_SOURCE_HEAT_PUMP);
-            if (response !== null) {
-                recommendations.push({ code: CODES.AIR_SOURCE_HEAT_PUMP, response });
-            }
-        }
-        if (shouldRecommendGroundSourceHeatPump(this.responseData)) {
-            const response = this.getBoilerTypeResponse(boilerTypes, BOILER_SLUGS.GROUND_SOURCE_HEAT_PUMP);
-            if (response !== null) {
-                recommendations.push({ code: CODES.GROUND_SOURCE_HEAT_PUMP, response });
-            }
-        }
-        return recommendations;
-    }
-
-    // TODO:SEA-38 Update recommendations with figures provided by BEIS.
-    private getInsulationRecommendations(): GreenHomesGrantRecommendation[] {
-        const recommendations: GreenHomesGrantRecommendation[] = [];
-        const { homeType, roofType } = this.responseData;
-        if (homeType === HomeType.ParkHomeOrMobileHome) {
-            recommendations.push({
-                code: CODES.PARK_HOME_INSULATION,
-                response: GreenHomesGrantRecommendationsService.getResponse(0, 0, 0)
-            });
-        } else if (roofType === RoofType.FlatNoInsulation) {
-            recommendations.push({
-                code: CODES.FLAT_ROOF_INSULATION,
-                response: GreenHomesGrantRecommendationsService.getResponse(0, 0, 0)
-            });
-        } else if (roofType === RoofType.PitchedNoInsulation || roofType === RoofType.PitchedInsulated) {
-            recommendations.push({
-                code: CODES.ROOM_IN_ROOF_INSULATION,
-                response: GreenHomesGrantRecommendationsService.getResponse(0, 0, 0)
-            });
-        }
-        return recommendations;
-    }
-
-    private getBoilerTypeResponse(boilerTypes: BoilerType[], slug: string): EnergySavingMeasureResponse {
-        const boilerType = boilerTypes.find(type => type.slug === slug);
-        if (boilerType === undefined) {
+    private static buildRecommendation(code: string, measureContents: MeasureContent[]): GreenHomesGrantRecommendation {
+        const measure = measureContents.find(m => m.acf.measure_code === code);
+        if (measure === undefined || measure.acf === undefined) {
             return null;
         }
-        const { installationCostLower, installationCostUpper, lifetimeYears } = boilerType;
-        return GreenHomesGrantRecommendationsService.getResponse(
-            installationCostLower,
-            installationCostUpper,
-            lifetimeYears
+        const response = GreenHomesGrantRecommendationsService.getResponse(
+            parseInt(measure.acf.installation_cost_lower_bound),
+            parseInt(measure.acf.installation_cost_upper_bound),
+            parseInt(measure.acf.lifetime)
         );
+        return { code, response };
     }
 
     private static getResponse(minInstallationCost: number, maxInstallationCost: number, lifetime: number): EnergySavingMeasureResponse {
         return {
             number: '',
-            min_installation_cost: minInstallationCost,
-            max_installation_cost: maxInstallationCost,
-            lifetime: lifetime,
+            min_installation_cost: minInstallationCost || 0,
+            max_installation_cost: maxInstallationCost || 0,
+            lifetime: lifetime || 0,
             energy_saving: 0,
             cost_saving: 0,
             uncertainty: 0,
