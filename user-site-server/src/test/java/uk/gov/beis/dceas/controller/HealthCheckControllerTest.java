4f79da7aa9628c30708fbe9355e808acd5861fbf
Robin Leach <robin.leach@softwire.com>
2020-10-07 17:18:54 +0100

    SEA-169: Add health check endpoint

---
+++ user-site-server/src/test/java/uk/gov/beis/dceas/controller/HealthCheckControllerTest.java
@@ -0,0 +1,28 @@
+package uk.gov.beis.dceas.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HealthCheckController.class)
+public class HealthCheckControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void healthCheckShouldReturn200() throws Exception {
+        mvc.perform(
+                get("/api/health")
+                        .with(user("username").password("password")))
+                .andExpect(
+                        status().isOk());
+    }
+}
