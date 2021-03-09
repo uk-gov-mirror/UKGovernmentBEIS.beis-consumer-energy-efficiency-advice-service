d1694cb310041a8a37dfdf5637bd7990259028c3
Laura Saunders <laura.saunders@softwire.com>
2021-03-09 10:09:53 +0000

    SEA-204: Markups

--- angular/src/app/shared/link-button/link-button.component.ts
+++ angular/src/app/shared/link-button/link-button.component.ts
@@ -1,8 +1,6 @@
 import {Component, Input} from '@angular/core';
 
 export type LinkButtonVariant = 'default' | 'green';
-export type NavigationType = 'router' | 'href';
-
 
 @Component({
     selector: 'app-link-button',
@@ -12,7 +10,6 @@ export type NavigationType = 'router' | 'href';
 export class LinkButtonComponent {
     @Input() buttonText: string;
     @Input() linkUrl: string;
-    @Input() type: NavigationType = 'href';
     @Input() centred?: boolean;
     @Input() variant: LinkButtonVariant = 'default';
     @Input() openInNewTab?: boolean;
